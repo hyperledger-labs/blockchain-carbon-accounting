d95f40556e0c59380843b8009633235e1cba4a13
Lam D. Nguyen <ndl@es.aau.dk>
2020-08-31 14:14:17 +0200

    Update README.md for golang chaincode version, computeEmissionAmount() is added.
    Add README for golang chaincode and updated compEmissionAmount function.
    
    @opentaps : can merge both READMEs into one version.
    initLedger func is just for testing. Can easily query data from couchdb.

--- utility-emissions-channel/chaincode/go/emissioncc.go
+++ utility-emissions-channel/chaincode/go/emissioncc.go
@@ -3,14 +3,17 @@
 package main
 
 import (
-	"fmt"
+	"bytes"
 	"encoding/json"
+	"fmt"
 	"strconv"
+	"time"
+
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 	pb "github.com/hyperledger/fabric/protos/peer"
 )
 
-// Define the structure of Chaincode
+/* Define the structure of Chaincode */
 
 type EmissionsContract struct {
 }
@@ -18,20 +21,22 @@ type EmissionsContract struct {
 /*Define structure of Carbon Accounting with 13 properties, these structures are used by encoding/json */
 
 type Value struct {
-	UtilityID      					string `json:"utilityID"`
-	PartyID      					string `json:"partyID"`
-	FromDate    					string `json:"fromDate"`
-	ThruDate      					string `json:"thruDate"`
-	EnergUseAmount 					string `json:"energyUseAmount"`
-	EnergyUSeUom 					string `json:"energyUSeUom"`
-	CO2equivalentemissions 				string `json:"cO2equivalentemissions"`
-	NetGeneration				 	string `json:"netGeneration"`
-	Usage				 		string `json:"usage"`
-	UsageuOM				 	string `json:"usageuOM"`
-	NetGenerationuOM				string `json:"netGenerationuOM"`
-	CO2equivalentemissionsuOM			string `json:"eO2equivalentemissionsuOM"`
-	EmissionsuOM				 	string `json:"emissionsuOM"`
+	UtilityID                 string `json:"utilityID"`
+	PartyID                   string `json:"partyID"`
+	FromDate                  string `json:"fromDate"`
+	ThruDate                  string `json:"thruDate"`
+	EnergUseAmount            string `json:"energyUseAmount"`
+	EnergyUSeUom              string `json:"energyUSeUom"`
+	CO2equivalentemissions    string `json:"cO2equivalentemissions"`
+	NetGeneration             string `json:"netGeneration"`
+	Usage                     string `json:"usage"`
+	UsageuOM                  string `json:"usageuOM"`
+	NetGenerationuOM          string `json:"netGenerationuOM"`
+	CO2equivalentemissionsuOM string `json:"eO2equivalentemissionsuOM"`
+	EmissionsuOM              string `json:"emissionsuOM"`
 }
+
+/* Compute Emission Amount */
 
 /* The Init Method is called when the chaincode is instiated by the BC */
 func (s *EmissionsContract) Init(APIstub shim.ChaincodeStubInterface) pb.Response {
@@ -53,20 +58,23 @@ func (s *EmissionsContract) Invoke(APIstub shim.ChaincodeStubInterface) pb.Respo
 		return s.getEmissionRecord(APIstub, args)
 	} else if function == "createEmissionRecord" {
 		return s.createEmissionRecord(APIstub, args)
-	} else if function == "compEmissionAmount"{
+	} else if function == "compEmissionAmount" {
 		return s.compEmissionAmount(APIstub, args)
+	} else if function == "getHistory" {
+		return s.getHistory(APIstub, args)
 	}
+
 	return shim.Error("Invalid Chaincode function name.")
 }
 
 /* InitLegder */
 func (s *EmissionsContract) initLedger(APIstub shim.ChaincodeStubInterface) pb.Response {
 	values := []Value{
-		Value{UtilityID: "Utility1", PartyID: "MyCOmpany1", FromDate: "2020-01-02", ThruDate: "2020-20-01", EnergUseAmount: "1650", EnergyUSeUom: "KWH", CO2equivalentemissions: "2528243",   NetGeneration: "5319362",   Usage: "3067",   UsageuOM: "4676",  NetGenerationuOM: "4676",  CO2equivalentemissionsuOM: "2514657",   EmissionsuOM: "11340"},
-		Value{UtilityID: "Utility2", PartyID: "MyCOmpany2", FromDate: "2020-01-02", ThruDate: "2020-20-01", EnergUseAmount: "1750", EnergyUSeUom: "KWH", CO2equivalentemissions: "109280652", NetGeneration: "233461835", Usage: "32448",  UsageuOM: "19846", NetGenerationuOM: "19846", CO2equivalentemissionsuOM: "108778338", EmissionsuOM: "41584"},
-		Value{UtilityID: "Utility3", PartyID: "MyCOmpany3", FromDate: "2020-01-02", ThruDate: "2020-20-01", EnergUseAmount: "1550", EnergyUSeUom: "KWH", CO2equivalentemissions: "7467070",   NetGeneration: "9553,749",   Usage: "3335",  UsageuOM: "9951",  NetGenerationuOM: "9951",  CO2equivalentemissionsuOM: "7412845",   EmissionsuOM: "22383"},
-		Value{UtilityID: "Utility4", PartyID: "MyCOmpany4", FromDate: "2020-01-02", ThruDate: "2020-20-01", EnergUseAmount: "1550", EnergyUSeUom: "KWH", CO2equivalentemissions: "271139570", NetGeneration: "455122654", Usage: "254855", UsageuOM: "88461", NetGenerationuOM: "88461", CO2equivalentemissionsuOM: "269299999", EmissionsuOM: "19224"}, 
-		Value{UtilityID: "Utility5", PartyID: "MyCOmpany5", FromDate: "2020-01-02", ThruDate: "2020-20-01", EnergUseAmount: "1550", EnergyUSeUom: "KWH", CO2equivalentemissions: "321014", 	  NetGeneration: "1167962",   Usage: "398",      UsageuOM: "3368",  NetGenerationuOM: "3368",  CO2equivalentemissionsuOM: "318764",     EmissionsuOM: "80027"},
+		Value{UtilityID: "Utility1", PartyID: "MyCOmpany1", FromDate: "2020-01-02", ThruDate: "2020-20-01", EnergUseAmount: "1650", EnergyUSeUom: "KWH", CO2equivalentemissions: "2543", NetGeneration: "5362", Usage: "3067", UsageuOM: "4676", NetGenerationuOM: "4676", CO2equivalentemissionsuOM: "257", EmissionsuOM: "140"},
+		Value{UtilityID: "Utility2", PartyID: "MyCOmpany2", FromDate: "2020-01-02", ThruDate: "2020-20-01", EnergUseAmount: "1750", EnergyUSeUom: "KWH", CO2equivalentemissions: "1062", NetGeneration: "235", Usage: "328", UsageuOM: "1846", NetGenerationuOM: "1946", CO2equivalentemissionsuOM: "1338", EmissionsuOM: "484"},
+		Value{UtilityID: "Utility3", PartyID: "MyCOmpany3", FromDate: "2020-01-02", ThruDate: "2020-20-01", EnergUseAmount: "1550", EnergyUSeUom: "KWH", CO2equivalentemissions: "7470", NetGeneration: "9549", Usage: "3335", UsageuOM: "9951", NetGenerationuOM: "951", CO2equivalentemissionsuOM: "7445", EmissionsuOM: "383"},
+		Value{UtilityID: "Utility4", PartyID: "MyCOmpany4", FromDate: "2020-01-02", ThruDate: "2020-20-01", EnergUseAmount: "1550", EnergyUSeUom: "KWH", CO2equivalentemissions: "2770", NetGeneration: "4554", Usage: "2855", UsageuOM: "8861", NetGenerationuOM: "881", CO2equivalentemissionsuOM: "2699", EmissionsuOM: "124"},
+		Value{UtilityID: "Utility5", PartyID: "MyCOmpany5", FromDate: "2020-01-02", ThruDate: "2020-20-01", EnergUseAmount: "1550", EnergyUSeUom: "KWH", CO2equivalentemissions: "3014", NetGeneration: "1162", Usage: "398", UsageuOM: "3368", NetGenerationuOM: "3368", CO2equivalentemissionsuOM: "318764", EmissionsuOM: "827"},
 	}
 	i := 0
 	for i < len(values) {
@@ -95,7 +103,7 @@ func (s *EmissionsContract) createEmissionRecord(APIstub shim.ChaincodeStubInter
 	return shim.Success(nil)
 }
 
-/* Query a value of Utility*/
+/* Query a Value of Utility*/
 
 func (s *EmissionsContract) getEmissionRecord(APIstub shim.ChaincodeStubInterface, args []string) pb.Response {
 	if len(args) != 1 {
@@ -103,32 +111,103 @@ func (s *EmissionsContract) getEmissionRecord(APIstub shim.ChaincodeStubInterfac
 	}
 
 	valuesAsBytes, _ := APIstub.GetState(args[0])
+
 	return shim.Success(valuesAsBytes)
 }
 
-/* Compute Emission Amount */ 
-
-func (s *EmissionsContract) compEmissionAmount(APIstub shim.ChaincodeStubInterface, args []string) pb.Response{
-	if len(args) !=1 {
-		return shim.Error("Utility ID is required")
+func (s *EmissionsContract) compEmissionAmount(APIstub shim.ChaincodeStubInterface, args []string) pb.Response {
+	if len(args) != 1 {
+		return shim.Error("In correct number of argument. Expect 1")
 	}
 
-	utilityID 		:= args[0]
-	valuesAsBytes, _ 	:= APIstub.GetState(args[0])
-	co2amount 		:= Value{}
-	json.UnMarshal(valuesAsBytes, co2amount)
+	valuesAsBytes, _ := APIstub.GetState(args[0])
 
-	cO2equivalentemissions 		  := args[6]
-	netGeneration 		  	  := args[7]
-	usage				  := args[8]
-	usageuOM 			  := args[9]
-	netGenerationuOM 	 	  := args[10]
-	cO2equivalentemissionsuOM	  := args[11]
-	emissionsuOM 			  := args[12]
+	record := Value{}
+	json.Unmarshal(valuesAsBytes, &record)
 
-	var emissionAmount = ((cO2equivalentemissions/netGeneration)*usage*(usageuOM/netGenerationuOM)*(cO2equivalentemissionsuOM/emissionsuOM))
+	cO2equivalentemissions := record.CO2equivalentemissions
+	netGeneration := record.NetGeneration
+	usage := record.Usage
+	usageuOM := record.UsageuOM
+	netGenerationuOM := record.NetGenerationuOM
+	cO2equivalentemissionsuOM := record.CO2equivalentemissionsuOM
+	emissionsuOM := record.EmissionsuOM
 
-	return emissionAmount
+	v1AsInt, _ := strconv.Atoi(cO2equivalentemissions)
+	v2AsInt, _ := strconv.Atoi(netGeneration)
+	v3AsInt, _ := strconv.Atoi(usage)
+	v4AsInt, _ := strconv.Atoi(usageuOM)
+	v5AsInt, _ := strconv.Atoi(netGenerationuOM)
+	v6AsInt, _ := strconv.Atoi(cO2equivalentemissionsuOM)
+	v7AsInt, _ := strconv.Atoi(emissionsuOM)
+
+	amount := v1AsInt / v2AsInt * v3AsInt * v4AsInt / v5AsInt * v6AsInt / v7AsInt
+
+	result := strconv.Itoa(amount) // convert int to string
+
+	mystringasBytes := []byte(result) // convert string to []byte
+
+	return shim.Success(mystringasBytes)
+}
+
+func (s *EmissionsContract) getHistory(APIstub shim.ChaincodeStubInterface, args []string) pb.Response {
+	if len(args) != 1 {
+		return shim.Error("invalid number of arguments")
+	}
+	utilityID := args[0]
+
+	iterator, err := APIstub.GetHistoryForKey(utilityID)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	iterator.Close()
+
+	// buffer is a JSON array containing QueryResults
+	var buffer bytes.Buffer
+	buffer.WriteString("[")
+	bArrayMemberAlreadyWritten := false
+	for iterator.HasNext() {
+		queryResponse, err := iterator.Next()
+		if err != nil {
+			shim.Error(err.Error())
+		}
+		// Add a comma before array members, suppress it for the first array member
+		if bArrayMemberAlreadyWritten == true {
+			buffer.WriteString(",")
+		}
+		buffer.WriteString("{\"TxId\":")
+		buffer.WriteString("\"")
+		buffer.WriteString(queryResponse.TxId)
+		buffer.WriteString("\"")
+
+		buffer.WriteString(", \"Value\":")
+		// if it was a delete operation on given key, then we need to set the
+		//corresponding value null. Else, we will write the response.Value
+		if queryResponse.IsDelete {
+			buffer.WriteString("null")
+		} else {
+			buffer.WriteString(string(queryResponse.Value))
+		}
+
+		buffer.WriteString(", \"Timestamp\":")
+		buffer.WriteString("\"")
+		buffer.WriteString(time.Unix(queryResponse.Timestamp.Seconds, int64(queryResponse.Timestamp.Nanos)).String())
+		buffer.WriteString("\"")
+
+		buffer.WriteString(", \"IsDelete\":")
+		buffer.WriteString("\"")
+		buffer.WriteString(strconv.FormatBool(queryResponse.IsDelete))
+		buffer.WriteString("\"")
+
+		buffer.WriteString("}")
+		bArrayMemberAlreadyWritten = true
+	}
+	buffer.WriteString("]")
+
+	fmt.Printf("getHistory:\n%s\n", buffer.String())
+
+	return shim.Success(buffer.Bytes())
 }
 
 /* main function */
@@ -139,4 +218,3 @@ func main() {
 		fmt.Printf("Error to start new SC", err)
 	}
 }
-
