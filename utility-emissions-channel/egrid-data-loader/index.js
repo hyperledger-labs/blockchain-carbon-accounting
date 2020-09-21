59b2bbfba66140ffc43e54e17085a389fc050dc2
Jeremy Wickersheimer <jwickers@gmail.com>
2020-09-21 16:47:18 +0800

    #3 load utility identifiers into database

--- utility-emissions-channel/egrid-data-loader/index.js
+++ utility-emissions-channel/egrid-data-loader/index.js
@@ -1,5 +1,6 @@
 const XLSX = require('xlsx');
 const NodeCouchDb = require('node-couchdb');
+const async = require('async');
 
 const DB_NAME = 'egrid_db';
 
@@ -18,7 +19,7 @@ yargs
   }, (argv) => {
     list_data(argv)
   })
-  .command('load <file> <sheet>', 'load data from XLSX file', (yargs) => {
+  .command('load_utility_emissions <file> <sheet>', 'load data from XLSX file', (yargs) => {
     yargs
       .positional('file', {
         describe: 'XLSX file to load from',
@@ -27,7 +28,18 @@ yargs
         describe: 'name of the worksheet to load from',
       })
   }, (argv) => {
-    parse_worksheet(argv.file, argv)
+    import_utility_emissions(argv.file, argv)
+  })
+  .command('load_utility_identifiers <file> [sheet]', 'load data from XLSX file', (yargs) => {
+    yargs
+      .positional('file', {
+        describe: 'XLSX file to load from',
+      })
+      .positional('sheet', {
+        describe: 'name of the worksheet to load from',
+      })
+  }, (argv) => {
+    import_utility_identifiers(argv.file, argv)
   })
   .option('username', {
     alias: 'u',
@@ -85,8 +97,7 @@ function deletedb(opts) {
 }
 
 
-function parse_worksheet(file_name, opts) {
-    const db = connectdb(opts);
+function parse_worksheet(file_name, opts, cb) {
     opts.verbose && console.log('Reading file ...  ', file_name);
     var workbook = XLSX.readFile(file_name);
 
@@ -101,6 +112,12 @@ function parse_worksheet(file_name, opts) {
         var worksheet = workbook.Sheets[y];
         var headers = {};
         var data = [];
+        var header_row = 1;
+        if (opts.skip_rows && opts.skip_rows >= header_row) {
+            header_row = opts.skip_rows + 1;
+        }
+        opts.verbose && console.log('-- opts.skip_rows = ', opts.skip_rows, ' header_row = ', header_row);
+
         for(z in worksheet) {
             if(z[0] === '!') continue;
             //parse out the column, row, and value
@@ -114,9 +131,11 @@ function parse_worksheet(file_name, opts) {
             var col = z.substring(0,tt).trim();
             var row = parseInt(z.substring(tt));
             var value = worksheet[z].v;
+            if (opts.skip_rows && opts.skip_rows >= row) continue;
+            //opts.verbose && console.log('--> ', row, col, value);
 
             //store header names
-            if(row == 1 && value) {
+            if(row == header_row && value) {
                 headers[col] = value;
                 continue;
             }
@@ -124,6 +143,13 @@ function parse_worksheet(file_name, opts) {
             if(!data[row]) data[row]={};
             data[row][headers[col]] = value;
         }
+        return cb(data);
+    });
+}
+
+function import_utility_emissions(file_name, opts) {
+    const db = connectdb(opts);
+    var data = parse_worksheet(file_name, opts, function(data) {
         // import data for each valid row, eg:
             // Year = 2018 from 'Data Year'
             // Country = USA
@@ -135,19 +161,15 @@ function parse_worksheet(file_name, opts) {
             // CO2_Equivalent_Emissions = value from 'NERC region annual CO2 equivalent emissions (tons)'
             // CO2_Equivalent_Emissions_UOM = tons
             // Source = https://www.epa.gov/sites/production/files/2020-01/egrid2018_all_files.zip
-        
-        //drop those first two rows which are empty
-        // data.shift();
-        // data.shift();
-        for (var i=0; i < data.length; i++) {
-            var row = data[i];
+        async.eachSeries(data, function iterator(row, callback) {
             // skip empty rows
-            if (!row || !row['Data Year']) continue;
+            if (!row || !row['Data Year']) return callback();
             // skip header rows
-            if (row['Data Year'] == 'YEAR') continue;
+            if (row['Data Year'] == 'YEAR') return callback();
             opts.verbose && console.log('-- Prepare to insert from ', row);
 
             var d = {};
+            d['DocType'] = 'UTILITY_EMISSION_FACTORS';
             d['Year'] = row['Data Year'];
             d['Country'] = 'USA';
             d['Division_type'] = 'NERC_REGION';
@@ -159,25 +181,50 @@ function parse_worksheet(file_name, opts) {
             d['CO2_Equivalent_Emissions_UOM'] = 'tons';
             d['Source'] = 'https://www.epa.gov/sites/production/files/2020-01/egrid2018_all_files.zip';
             // generate a unique for the row
-            var document_id = d['Country'] + '_' + d['Year'] + '_' + d['Division_type'] + '_' + d['Division_id'];
+            var document_id = d['DocType'] + '_' + d['Country'] + '_' + d['Year'] + '_' + d['Division_type'] + '_' + d['Division_id'];
             d['_id'] = document_id;
 
-            db.insert(DB_NAME, d).then(({data, headers, status}) => {
-                // data is json response
-                // headers is an object with all response headers
-                // status is statusCode number
-                opts.verbose && console.log('** INSERT: ', status, data, headers);
-            }, err => {
-                // either request error occured
-                // ...or err.code=EDOCCONFLICT if document with the same id already exists
-                if (err.code == 'EDOCCONFLICT') {
-                    console.error('Row already exists');
-                } else {
-                    console.error(err);
-                }
-            });
-        }
-        //opts.verbose && console.log(data);
+            couchdb_insert(db, opts, d, callback);
+        });
+
+    });
+}
+
+function import_utility_identifiers(file_name, opts) {
+    const db = connectdb(opts);
+    opts.skip_rows = 2
+    var data = parse_worksheet(file_name, opts, function(data) {
+        // import data for each valid row, eg:
+            // Utility_Number = value from 'Utility Number'
+            // Utility_Name = value from 'Utility Name'
+            // State_Province = value from 'State'
+            // Country = USA
+            // Divisions = an array of bojects
+            // -- Division_type = NERC_REGION
+            // -- Division_id = value from 'NERC Region'
+        async.eachSeries(data, function iterator(row, callback) {
+            if (!row || !row['Data Year']) return callback();
+
+            opts.verbose && console.log('-- Prepare to insert from ', row);
+
+            var d = {};
+            d['DocType'] = 'UTILITY_LOOKUP';
+            d['Utility_Number'] = row['Utility Number'];
+            d['Utility_Name'] = row['Utility Name'];
+            d['Country'] = 'USA';
+            d['State_Province'] = row['State'];
+            d['Divisions'] = [{
+                'Division_type': 'NERC_REGION',
+                'Division_id': row['NERC region']
+            }];
+            // generate a unique for the row
+            d['_id'] = d['DocType'] + '_' + d['Utility_Number'];
+            opts.verbose && console.log('-- TRY INSERT ', d);
+
+            couchdb_insert(db, opts, d, callback);
+
+        });
+
     });
 }
 
@@ -195,5 +242,43 @@ function list_data(opts) {
         // ...or err.code=EDOCMISSING if document is missing
         // ...or err.code=EUNKNOWN if statusCode is unexpected
         console.error(err);
+    });
+}
+
+function _couchdb_insert(db, opts, d, callback) {
+    db.insert(DB_NAME, d).then(({data, headers, status}) => {
+        // data is json response
+        // headers is an object with all response headers
+        // status is statusCode number
+        opts.verbose && console.log('** INSERT: ', status, data, headers);
+        console.log('Imported document [' + d['_id']  + ']')
+        return callback();
+    }, err => {
+        // either request error occured
+        // ...or err.code=EDOCCONFLICT if document with the same id already exists
+        if (err.code == 'EDOCCONFLICT') {
+            console.error('Document [' + d['_id']  + '] already exists');
+        } else {
+            console.error('ERROR Inserting Document [' + d['_id']  + '] -- Retrying');
+            return callback(err);
+        }
+        return callback();
+    });
+}
+
+function couchdb_insert(db, opts, d, callback) {
+    // Note: on large imports this can overload the DB with opened file as
+    // the data is written to disk, to workaround that wrap the insert into 
+    // a retry loop
+    // try calling apiMethod 10 times with exponential backoff
+    // (i.e. intervals of 100, 200, 400, 800, 1600, ... milliseconds)
+    async.retry({
+      times: 5,
+      interval: function(retryCount) {
+        return 50 * Math.pow(2, retryCount);
+      }
+    }, cb => { _couchdb_insert(db, opts,d, cb) }, err => {
+        if (err) console.error('ERROR Inserting Document [' + d['_id']  + ']', err);
+        return callback(err);
     });
 }
