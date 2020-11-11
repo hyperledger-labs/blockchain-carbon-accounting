c96309f2deed340dec8169d2a7d41e2969b62c57
Michael Bauer <michaelbauerinc@gmail.com>
2020-11-11 13:09:38 -0500

    fix startNetwork.sh

--- utility-emissions-channel/docker-compose-setup/scripts/startNetwork.sh
+++ utility-emissions-channel/docker-compose-setup/scripts/startNetwork.sh
@@ -5,6 +5,7 @@ EXISTS=$(docker network inspect $(docker network ls -q) | grep "carbonAccounting
 if [ -z "$EXISTS" ]; then
     # start docker network
     echo "Starting docker network with name $NETWORK_NAME"
+    docker swarm init
     docker network create --attachable --driver overlay $NETWORK_NAME
 else
     echo "Network $NETWORK_NAME already exists, skipping creation..."
