72da56fd7f3a93a397214bbfbd7888a7c8825fb3
Jameson Hodge <jamesondh@gmail.com>
2020-12-03 11:00:33 -0500

    WIP - connect interface to test greeter contract on local hardhat testnet
    Signed-off-by: Jameson Hodge <jamesondh@gmail.com>

--- net-emissions-token-network/interface/packages/react-app/src/App.js
+++ net-emissions-token-network/interface/packages/react-app/src/App.js
@@ -11,15 +11,22 @@ import useWeb3Modal from "./hooks/useWeb3Modal";
 import { addresses, abis } from "@project/contracts";
 import GET_TRANSFERS from "./graphql/subgraph";
 
-async function readOnChainData() {
-  // Should replace with the end-user wallet, e.g. Metamask
-  const defaultProvider = getDefaultProvider();
+async function readOnChainData(w3provider) {
   // Create an instance of an ethers.js Contract
-  // Read more about ethers.js on https://docs.ethers.io/v5/api/contract/contract/
-  const ceaErc20 = new Contract(addresses.ceaErc20, abis.erc20, defaultProvider);
-  // A pre-defined address that owns some CEAERC20 tokens
-  const tokenBalance = await ceaErc20.balanceOf("0x3f8CB69d9c0ED01923F11c829BaE4D9a4CB6c82C");
-  console.log({ tokenBalance: tokenBalance.toString() });
+  console.log(abis.greeter.abi)
+  // The address below is copied after deploying the greeter contract to the local hardhat network
+  const contract_greeter = new Contract("0x5FbDB2315678afecb367f032d93F642f64180aa3", abis.greeter.abi, w3provider);
+  const greeting = await contract_greeter.greet(); // returns error
+  console.log({ greeting: greeting.toString() });
+
+  // Get balance of test address on hardhat
+  //
+  // console.log(w3provider);
+  // let balancePromise = w3provider.getBalance("0xdd2fd4581271e230360230f9337d5c0430bf44c0");
+  // // let balancePromise = w3provider.getBlockNumber();
+  // balancePromise.then(function(balance) {
+  //   console.log(balance);
+  // });
 }
 
 function App() {
@@ -40,8 +47,8 @@ function App() {
           Edit <code>packages/react-app/src/App.js</code> and save to reload.
         </p>
         {/* Remove the "hidden" prop and open the JavaScript console in the browser to see what this function does */}
-        <Button hidden onClick={() => readOnChainData()}>
-          Read On-Chain Balance
+        <Button onClick={() => readOnChainData(provider)}>
+          Return greeting
         </Button>
       </Container>
     </div>
