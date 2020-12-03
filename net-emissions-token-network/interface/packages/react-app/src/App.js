da1a95aef57086448ea15a8eb1e33b1d94de8dfe
Jameson Hodge <jamesondh@gmail.com>
2020-12-03 14:55:15 -0500

    Add routing and navigation menu
    Signed-off-by: Jameson Hodge <jamesondh@gmail.com>

--- net-emissions-token-network/interface/packages/react-app/src/App.js
+++ net-emissions-token-network/interface/packages/react-app/src/App.js
@@ -5,15 +5,21 @@ import { useQuery } from "@apollo/react-hooks";
 
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Tab from 'react-bootstrap/Tab';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 import { NavigationBar } from "./components/navigation-bar";
 import useWeb3Modal from "./hooks/useWeb3Modal";
+
+import { Link, Route, Switch, Redirect } from "wouter"
 
 import { addresses, abis } from "@project/contracts";
 import GET_TRANSFERS from "./graphql/subgraph";
 
 async function readOnChainData(w3provider) {
   // Create an instance of an ethers.js Contract
-  console.log(abis.greeter.abi)
   // The address below is copied after deploying the greeter contract to the local hardhat network
   const contract_greeter = new Contract("0x5FbDB2315678afecb367f032d93F642f64180aa3", abis.greeter.abi, w3provider);
   const greeting = await contract_greeter.greet(); // returns error
@@ -42,14 +48,32 @@ function App() {
   return (
     <div>
       <NavigationBar provider={provider} loadWeb3Modal={loadWeb3Modal} logoutOfWeb3Modal={logoutOfWeb3Modal}/>
-      <Container>
-        <p>
-          Edit <code>packages/react-app/src/App.js</code> and save to reload.
-        </p>
-        {/* Remove the "hidden" prop and open the JavaScript console in the browser to see what this function does */}
-        <Button onClick={() => readOnChainData(provider)}>
-          Return greeting
-        </Button>
+      <Container className="mt-2">
+
+        <Tab.Container defaultActiveKey="test">
+          <Row>
+            <Col sm={3}>
+              <Nav variant="pills" className="flex-column">
+                <Link href="test"><Nav.Link eventKey="test">Test</Nav.Link></Link>
+                <Link href="mint"><Nav.Link eventKey="mint">Mint</Nav.Link></Link>
+              </Nav>
+            </Col>
+            <Col sm={9}>
+              <Tab.Content animation="true">
+                <Switch>
+                  <Route exact path="/"><Redirect to="/test" /></Route>
+                  <Route path="/test">
+                    <Button onClick={() => readOnChainData(provider)}>
+                      Return greeting
+                    </Button>
+                  </Route>
+                  <Route path="/mint">...</Route>
+                </Switch>
+              </Tab.Content>
+            </Col>
+          </Row>
+        </Tab.Container>
+
       </Container>
     </div>
   );
