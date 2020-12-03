573af7ea1fc0c929a746cd4428028f041c10dc5f
Jameson Hodge <jamesondh@gmail.com>
2020-12-03 11:00:33 -0500

    WIP - connect interface to test greeter contract on local hardhat testnet
    Signed-off-by: Jameson Hodge <jamesondh@gmail.com>

--- net-emissions-token-network/interface/packages/contracts/src/abis.js
+++ net-emissions-token-network/interface/packages/contracts/src/abis.js
@@ -1,9 +1,11 @@
 import erc20Abi from "./abis/erc20.json";
 import ownableAbi from "./abis/ownable.json";
+import greeterAbi from "./abis/Greeter.json";
 
 const abis = {
   erc20: erc20Abi,
   ownable: ownableAbi,
+  greeter: greeterAbi
 };
 
 export default abis;
