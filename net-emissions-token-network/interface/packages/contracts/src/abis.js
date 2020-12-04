602cd4df33bfd1728410d369ebac7f6a341f99a1
Jameson Hodge <jamesondh@gmail.com>
2020-12-04 12:15:25 -0500

    Export abi to interface
    Signed-off-by: Jameson Hodge <jamesondh@gmail.com>

--- net-emissions-token-network/interface/packages/contracts/src/abis.js
+++ net-emissions-token-network/interface/packages/contracts/src/abis.js
@@ -1,11 +1,13 @@
 import erc20Abi from "./abis/erc20.json";
 import ownableAbi from "./abis/ownable.json";
 import greeterAbi from "./abis/Greeter.json";
+import netEmissionsTokenNetworkAbi from "./abis/NetEmissionsTokenNetwork.json";
 
 const abis = {
   erc20: erc20Abi,
   ownable: ownableAbi,
-  greeter: greeterAbi
+  greeter: greeterAbi,
+  netEmissionsTokenNetwork: netEmissionsTokenNetworkAbi
 };
 
 export default abis;
