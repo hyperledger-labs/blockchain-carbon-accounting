d5e37c2368eaf23361e263dd7b16cd051f8fcbb6
Jameson Hodge <jamesondh@gmail.com>
2020-12-10 08:24:50 -0500

    Use correct ABI; fix params for issue form on interface
    Signed-off-by: Jameson Hodge <jamesondh@gmail.com>

--- net-emissions-token-network/interface/packages/react-app/src/components/issue-form.js
+++ net-emissions-token-network/interface/packages/react-app/src/components/issue-form.js
@@ -11,26 +11,24 @@ export function IssueForm({ provider }) {
   const [address, setAddress] = useState("");
   const [tokenTypeId, setTokenTypeId] = useState("");
   const [quantity, setQuantity] = useState("");
-  const [issuerId, setIssuerId] = useState("");
-  const [recipientId, setRecipientId] = useState("");
   const [uom, setUom] = useState("");
   const [fromDate, setFromDate] = useState("");
   const [thruDate, setThruDate] = useState("");
   const [metadata, setMetadata] = useState("");
   const [manifest, setManifest] = useState("");
   const [automaticRetireDate, setAutomaticRetireDate] = useState("");
+  const [description, setDescription] = useState("");
   const [result, setResult] = useState("");
 
   function onAddressChange(event) { setAddress(event.target.value); };
   function onQuantityChange(event) { setQuantity(event.target.value); };
-  function onIssuerIdChange(event) { setIssuerId(event.target.value); };
-  function onRecipientIdChange(event) { setRecipientId(event.target.value); };
   function onUomChange(event) { setUom(event.target.value); };
   function onFromDateChange(event) { setFromDate(event.target.value); };
   function onThruDateChange(event) { setThruDate(event.target.value); };
   function onMetadataChange(event) { setMetadata(event.target.value); };
   function onManifestChange(event) { setManifest(event.target.value); };
   function onAutomaticRetireDateChange(event) { setAutomaticRetireDate(event.target.value); };
+  function onDescriptionChange(event) { setDescription(event.target.value); };
 
   async function submit(w3provider) {
     let signer = w3provider.getSigner();
@@ -38,19 +36,19 @@ export function IssueForm({ provider }) {
     let signed = await contract.connect(signer);
     let issue_result;
     try {
-      let addcarbontoken_result_raw = await signed.issue(
+      let issue_result_raw = await signed.issue(
         address,
+        tokenTypeId,
         quantity,
-        issuerId,
-        recipientId,
         uom,
         fromDate,
         thruDate,
         metadata,
         manifest,
-        automaticRetireDate
+        automaticRetireDate,
+        description
       );
-      issue_result = addcarbontoken_result_raw.message;
+      issue_result = issue_result_raw.message;
     } catch (error) {
       console.error("Error calling issue()")
       issue_result = error.message;
@@ -61,6 +59,7 @@ export function IssueForm({ provider }) {
 
   return (
     <>
+      <h2>Issue tokens</h2>
       <Form.Group>
         <Form.Label>Address</Form.Label>
         <Form.Control type="input" placeholder="0x000..." value={address} onChange={onAddressChange} />
@@ -76,14 +75,6 @@ export function IssueForm({ provider }) {
       <Form.Group>
         <Form.Label>Quantity</Form.Label>
         <Form.Control type="input" placeholder="100" value={quantity} onChange={onQuantityChange} />
-      </Form.Group>
-      <Form.Group>
-        <Form.Label>Issuer ID</Form.Label>
-        <Form.Control type="input" placeholder="0x000..." value={issuerId} onChange={onIssuerIdChange} />
-      </Form.Group>
-      <Form.Group>
-        <Form.Label>Recipient ID</Form.Label>
-        <Form.Control type="input" placeholder="0x000..." value={recipientId} onChange={onRecipientIdChange} />
       </Form.Group>
       <Form.Group>
         <Form.Label>UOM</Form.Label>
@@ -108,6 +99,10 @@ export function IssueForm({ provider }) {
       <Form.Group>
         <Form.Label>Automatic Retire Date</Form.Label>
         <Form.Control type="input" placeholder="" value={automaticRetireDate} onChange={onAutomaticRetireDateChange} />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>Description</Form.Label>
+        <Form.Control as="textarea" placeholder="" value={description} onChange={onDescriptionChange} />
       </Form.Group>
       <Button variant="primary" size="lg" block onClick={() => submit(provider)}>
         Submit
