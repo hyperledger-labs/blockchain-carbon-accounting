927afe03b5ff91bc33df26139bb89174f8717778
Jameson Hodge <jamesondh@gmail.com>
2020-12-16 18:15:20 -0500

    Create retire tokens component/page; fetch retire() contract function from interface
    Signed-off-by: Jameson Hodge <jamesondh@gmail.com>

---
+++ net-emissions-token-network/interface/packages/react-app/src/components/retire-form.js
@@ -0,0 +1,97 @@
+import React, { useState } from "react";
+
+import { retire } from "../services/contract-functions";
+
+import SubmissionModal from "./submission-modal";
+
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Datetime from "react-datetime";
+import "react-datetime/css/react-datetime.css";
+
+export default function RetireForm({ provider }) {
+
+  const [modalShow, setModalShow] = useState(false);
+
+  // Form inputs
+  const [address, setAddress] = useState("");
+  const [tokenId, setTokenId] = useState();
+  const [amount, setAmount] = useState("");
+  const [result, setResult] = useState("");
+
+  // After initial onFocus for required inputs, display red outline if invalid
+  const [initializedAddressInput, setInitializedAddressInput] = useState(false);
+  const [initializedTokenIdInput, setInitializedTokenIdInput] = useState(false);
+  const [initializedAmountInput, setInitializedAmountInput] = useState(false);
+
+  function onAddressChange(event) { setAddress(event.target.value); };
+  function onTokenIdChange(event) { setTokenId(event.target.value); };
+  function onAmountChange(event) { setAmount(event.target.value); };
+
+  function handleRetire() {
+    fetchRetire();
+    setModalShow(true);
+  }
+
+  async function fetchRetire() {
+    let result = await retire(provider, address, tokenId, amount);
+    setResult(result.toString());
+  }
+
+  const inputError = {
+    boxShadow: '0 0 0 0.2rem rgba(220,53,69,.5)',
+    borderColor: '#dc3545'
+  };
+
+  return (
+    <>
+
+      <SubmissionModal
+        show={modalShow}
+        title="Retire tokens"
+        body={result}
+        onHide={() => {setModalShow(false); setResult("")} }
+      />
+
+      <h2>Retire tokens</h2>
+      <Form.Group>
+        <Form.Label>Address</Form.Label>
+        <Form.Control
+          type="input"
+          placeholder="0x000..."
+          value={address}
+          onChange={onAddressChange}
+          onBlur={() => setInitializedAddressInput(true)}
+          style={(address || !initializedAddressInput) ? {} : inputError}
+        />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>Token ID</Form.Label>
+        <Form.Control
+          type="input"
+          placeholder="1, 2, 3, ..."
+          value={tokenId}
+          onChange={onTokenIdChange}
+          onBlur={() => setInitializedTokenIdInput(true)}
+          style={(tokenId || !initializedTokenIdInput) ? {} : inputError}
+        />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>Quantity</Form.Label>
+        <Form.Control
+          type="input"
+          placeholder="100"
+          value={amount}
+          onChange={onAmountChange}
+          onBlur={() => setInitializedAmountInput(true)}
+          style={(amount || !initializedAmountInput) ? {} : inputError}
+        />
+      </Form.Group>
+      <Button variant="primary" size="lg" block onClick={handleRetire}>
+        Submit
+      </Button>
+    </>
+  );
+}
