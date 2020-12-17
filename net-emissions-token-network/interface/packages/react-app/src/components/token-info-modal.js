8702f448fc6aeffc2aa7db340a256af80a29f440
Jameson Hodge <jamesondh@gmail.com>
2020-12-17 10:26:56 -0500

    Display retired balance in token details modal; remove redundant token type formatting in modal
    Signed-off-by: Jameson Hodge <jamesondh@gmail.com>

--- net-emissions-token-network/interface/packages/react-app/src/components/token-info-modal.js
+++ net-emissions-token-network/interface/packages/react-app/src/components/token-info-modal.js
@@ -17,20 +17,6 @@ export default function TokenInfoModal(props) {
     // Fetch raw token details
     let details_raw = await getTokenDetails(props.provider, props.token.id);
 
-    // Format token type ID
-    let tokenType;
-    switch (details_raw.tokenTypeId) {
-      case 1:
-        tokenType = "Renewable Energy Certificate";
-        break;
-      case 2:
-        tokenType = "Carbon Emissions Offset";
-        break;
-      case 3:
-        tokenType = "Audited Emissions";
-        break;
-    }
-
     // Format unix times to Date objects
     let fromDateObj = new Date((details_raw.fromDate.toNumber()) * 1000);
     let thruDateObj = new Date((details_raw.thruDate.toNumber()) * 1000);
@@ -40,7 +26,7 @@ export default function TokenInfoModal(props) {
       tokenId: details_raw.tokenId,
       issuer: details_raw.issuer,
       issuee: details_raw.issuee,
-      // tokenTypeId: tokenType,
+      retiredAmount: details_raw.retiredAmount.toNumber(),
       uom: details_raw.uom,
       fromDate: fromDateObj.toLocaleString(),
       thruDate: thruDateObj.toLocaleString(),
@@ -112,6 +98,10 @@ export default function TokenInfoModal(props) {
                   <tr>
                     <td>Issuee</td>
                     <td>{tokenDetails.issuee}</td>
+                  </tr>
+                  <tr>
+                    <td>Total retired amount</td>
+                    <td>{tokenDetails.retiredAmount}</td>
                   </tr>
                   <tr>
                     <td>UOM</td>
