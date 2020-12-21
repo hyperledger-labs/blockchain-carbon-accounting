b816a802e42619aff70c5eff95985b4f51c1e768
Jameson Hodge <contact@jamesonhodge.com>
2020-12-21 13:42:52 -0500

    Remove unused ABI references from interface
    Signed-off-by: Jameson Hodge <contact@jamesonhodge.com>

--- net-emissions-token-network/interface/packages/contracts/src/abis.js
+++ net-emissions-token-network/interface/packages/contracts/src/abis.js
@@ -1,10 +1,6 @@
-import erc20Abi from "./abis/erc20.json";
-import ownableAbi from "./abis/ownable.json";
 import netEmissionsTokenNetworkAbi from "./abis/NetEmissionsTokenNetwork.json";
 
 const abis = {
-  erc20: erc20Abi,
-  ownable: ownableAbi,
   netEmissionsTokenNetwork: netEmissionsTokenNetworkAbi
 };
 
