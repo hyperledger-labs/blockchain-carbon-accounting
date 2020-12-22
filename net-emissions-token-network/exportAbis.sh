4fe856742d5147019052eacbd8fb42f00bc123b1
Jameson Hodge <contact@jamesonhodge.com>
2020-12-22 10:58:53 -0500

    Create shell script for copying ABIs to interface and utility emissions channel folder
    Signed-off-by: Jameson Hodge <contact@jamesonhodge.com>

---
+++ net-emissions-token-network/exportAbis.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cp ./artifacts/contracts/NetEmissionsTokenNetwork.sol/NetEmissionsTokenNetwork.json ./interface/packages/contracts/src/abis/
+cp ./artifacts/contracts/NetEmissionsTokenNetwork.sol/NetEmissionsTokenNetwork.json ../utility-emissions-channel/typescript_app/src/blockchain-gateway/net-emissions-token-network/
