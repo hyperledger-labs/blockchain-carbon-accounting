11976156f47995a79eb80f1e5c1a3ddd2e56f3a4
Jameson Hodge <contact@jamesonhodge.com>
2020-12-25 10:05:14 -0500

    Install hardhat gas reporter to output gas limits on npx hardhat test
    Signed-off-by: Jameson Hodge <contact@jamesonhodge.com>

--- net-emissions-token-network/hardhat.config.js
+++ net-emissions-token-network/hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
 require("solidity-coverage");
+require("hardhat-gas-reporter");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
