a0bfcb9f2056d3a0fdd0d400ea52deb08c2a6ccb
Michael Bauer <michaelbauerinc@gmail.com>
2020-12-29 09:32:46 -0500

    recording audited emissions token from ledger data/api fully functional

---
+++ net-emissions-token-network/runDockerHardhatTestNet.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+VOLUME=$(pwd)
+NETWORK_NAME="carbonAccounting"
+IMAGE=$(docker images hardhat-test --format "{{.Repository}}")
+if [ "$IMAGE" ]; then
+    echo "Found hardhat-test image. No need to build.";
+else
+    echo "Building hardhat-test image"
+    docker build -t hardhat-test .
+fi
+echo "Using volumes for hardhat-test container: "
+echo
+echo $VOLUME
+
+docker run -it --rm --name hardhat-test --network=$NETWORK_NAME -v $VOLUME:/net-emissions-token-network -w /net-emissions-token-network hardhat-test /bin/sh -c 'npx hardhat node --hostname 0.0.0.0'
+
+
+
