34c83c0e12b541a4abb2fde628fa37c24fb0f6b0
Jameson Hodge <contact@jamesonhodge.com>
2021-01-06 10:44:57 -0500

    Misc cleanup and removal of warnings in react console
    Signed-off-by: Jameson Hodge <contact@jamesonhodge.com>

--- net-emissions-token-network/interface/packages/react-app/src/components/retire-form.js
+++ net-emissions-token-network/interface/packages/react-app/src/components/retire-form.js
@@ -6,9 +6,6 @@ import SubmissionModal from "./submission-modal";
 
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Datetime from "react-datetime";
 import "react-datetime/css/react-datetime.css";
 
 export default function RetireForm({ provider }) {
--- net-emissions-token-network/interface/packages/react-app/src/components/submission-modal.js
+++ net-emissions-token-network/interface/packages/react-app/src/components/submission-modal.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
