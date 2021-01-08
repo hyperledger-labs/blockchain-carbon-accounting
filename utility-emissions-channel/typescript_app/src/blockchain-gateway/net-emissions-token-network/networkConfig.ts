0533c790fdd7351fa3a1099c5ce6f239acb3cf70
Jameson Hodge <contact@jamesonhodge.com>
2021-01-08 08:50:16 -0500

    Hide networkConfig.ts by default for typescript API; create networkConfig.ts.example
    Signed-off-by: Jameson Hodge <contact@jamesonhodge.com>

--- utility-emissions-channel/typescript_app/src/blockchain-gateway/net-emissions-token-network/networkConfig.ts
+++
@@ -1,3 +0,0 @@
-export const privateKey = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
-export const contractAddress = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
-export const url = "http://hardhat-test:8545";
