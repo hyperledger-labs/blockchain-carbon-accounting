cd2000971f17b2adbb2c7ee8d9de7b8d730a8029
Jameson Hodge <contact@jamesonhodge.com>
2021-01-12 15:51:31 -0500

    Only display roles user has in list in manage roles page; improve formatting
    Signed-off-by: Jameson Hodge <contact@jamesonhodge.com>

--- net-emissions-token-network/interface/packages/react-app/src/components/access-control-form.js
+++ net-emissions-token-network/interface/packages/react-app/src/components/access-control-form.js
@@ -12,6 +12,23 @@ import Col from 'react-bootstrap/Col';
 import InputGroup from 'react-bootstrap/InputGroup';
 import FormControl from 'react-bootstrap/FormControl';
 
+import { FaCheck } from 'react-icons/fa'
+
+function RolesListElements({ roles }) {
+  const roleNames = ["Owner", "REC Dealer", "Offset Dealer", "Emissions Auditor", "Consumer"];
+  return roles.map((role, id) => 
+    <>{role && <li className="list-group-item" key="role">{roleNames[id]}&nbsp;&nbsp;<span className="text-success"><FaCheck/></span></li>}</>
+  );
+}
+
+function RolesList({ roles }) {
+  return (
+    <ul className="list-group pt-1 pb-3">
+      <RolesListElements roles={roles}/>
+    </ul>
+  );
+}
+
 export default function AccessControlForm({ provider, signedInAddress, roles }) {
 
   const [modalShow, setModalShow] = useState(false);
@@ -23,14 +40,6 @@ export default function AccessControlForm({ provider, signedInAddress, roles }) 
   // Fetching roles of outside address
   const [theirAddress, setTheirAddress] = useState();
   const [theirRoles, setTheirRoles] = useState([]);
-
-  function xOrCheck(value) {
-    if (value) {
-      return <span className="text-success">✔</span>;
-    } else {
-      return <span className="text-danger">✖</span>;
-    }
-  }
 
   async function fetchTheirRoles() {
     let result = await getRoles(provider, theirAddress);
@@ -114,42 +123,35 @@ export default function AccessControlForm({ provider, signedInAddress, roles }) 
       <h2>Manage roles</h2>
       <p>Register or unregister roles for different addresses on the network. Must be an owner to register dealers, and must be a dealer to register consumers.</p>
 
-      <h4>My Roles</h4>
-      {roles.length === 5 ? 
-        <Row className="text-center mb-3">
-          <Col className="align-self-center"><small>Owner</small> {xOrCheck(roles[0])}</Col>
-          <Col className="align-self-center"><small>REC Dealer</small> {xOrCheck(roles[1])}</Col>
-          <Col className="align-self-center"><small>Offset Dealer</small> {xOrCheck(roles[2])}</Col>
-          <Col className="align-self-center"><small>Emissions Auditor</small> {xOrCheck(roles[3])}</Col>
-          <Col className="align-self-center"><small>Consumer</small> {xOrCheck(roles[4])}</Col>
-        </Row>
-        : 
-        <div className="text-center mt-3 mb-3">
-          <Spinner animation="border" role="status">
-            <span className="sr-only">Loading...</span>
-          </Spinner>
-        </div>
-      }
-
-      <h4>Look-up Roles</h4>
-      <InputGroup className="mb-3">
-        <FormControl
-          placeholder="0x000..."
-          onChange={onTheirAddressChange}
-        />
-        <InputGroup.Append>
-          <Button variant="outline-secondary" onClick={fetchTheirRoles}>Look-up</Button>
-        </InputGroup.Append>
-      </InputGroup>
-      {theirRoles.length === 5 &&
-        <Row className="text-center mb-3">
-          <Col className="align-self-center"><small>Owner</small> {xOrCheck(theirRoles[0])}</Col>
-          <Col className="align-self-center"><small>REC Dealer</small> {xOrCheck(theirRoles[1])}</Col>
-          <Col className="align-self-center"><small>Offset Dealer</small> {xOrCheck(theirRoles[2])}</Col>
-          <Col className="align-self-center"><small>Emissions Auditor</small> {xOrCheck(theirRoles[3])}</Col>
-          <Col className="align-self-center"><small>Consumer</small> {xOrCheck(theirRoles[4])}</Col>
-        </Row>
-      }
+      <Row>
+        <Col>
+          <h4>My Roles</h4>
+          {roles.length === 5 ? 
+            <RolesList roles={roles}/>
+            : 
+            <div className="text-center mt-3 mb-3">
+              <Spinner animation="border" role="status">
+                <span className="sr-only">Loading...</span>
+              </Spinner>
+            </div>
+          }
+        </Col>
+        <Col>
+          <h4>Look-up Roles</h4>
+          <InputGroup className="mb-3">
+            <FormControl
+              placeholder="0x000..."
+              onChange={onTheirAddressChange}
+            />
+            <InputGroup.Append>
+              <Button variant="outline-secondary" onClick={fetchTheirRoles}>Look-up</Button>
+            </InputGroup.Append>
+          </InputGroup>
+          {theirRoles.length === 5 &&
+            <RolesList roles={theirRoles}/>
+          }
+        </Col>
+      </Row>
 
       {/* Only display registration/unregistration tokens if owner or dealer */}
       {(roles[0] === true || roles[1] === true || roles[2] === true || roles[3] === true) &&
