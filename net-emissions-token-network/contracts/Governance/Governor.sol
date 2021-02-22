66784d32c486529a1dcde38a589bc557d17d1d5e
Jameson Hodge <contact@jamesonhodge.com>
2021-02-22 12:06:49 -0500

    net-emissions-token-network - WIP on integration test for creating proposal from DAO contracts
    Signed-off-by: Jameson Hodge <contact@jamesonhodge.com>

--- net-emissions-token-network/contracts/Governance/Governor.sol
+++ net-emissions-token-network/contracts/Governance/Governor.sol
@@ -33,10 +33,10 @@ contract Governor {
     string public constant name = "Compound Governor Alpha";
 
     /// @notice The number of votes in support of a proposal required in order for a quorum to be reached and for a vote to succeed
-    function quorumVotes() public pure returns (uint) { return 400000e18; } // 400,000 = 4% of Comp
+    function quorumVotes() public pure returns (uint) { return 400000e18; } // 400,000 = 4% of Daot
 
     /// @notice The number of votes required in order for a voter to become a proposer
-    function proposalThreshold() public pure returns (uint) { return 100000e18; } // 100,000 = 1% of Comp
+    function proposalThreshold() public pure returns (uint) { return 100000e18; } // 100,000 = 1% of Daot
 
     /// @notice The maximum number of actions that can be included in a proposal
     function proposalMaxOperations() public pure returns (uint) { return 10; } // 10 actions
--- net-emissions-token-network/test/DAO-integration-tests.js
+++ net-emissions-token-network/test/DAO-integration-tests.js
@@ -1,5 +1,6 @@
 const { expect } = require("chai");
 const {
+  deployContract,
   deployDaoContracts
 } = require("./common.js");
 
@@ -33,6 +34,70 @@ describe("Climate DAO - Integration tests", function() {
     let balanceOfDaoUserAfterTransfer = await contracts.daoToken
       .balanceOf(DAOuser.address)
       .then((response) => expect(response.toString()).to.equal('1000000'));
+
+  });
+
+  it("should allow a user with enough DAO tokens to submit a proposal", async function() {
+    const contracts = await deployDaoContracts();
+    const netEmissionsTokenNetwork = await deployContract("NetEmissionsTokenNetwork");
+
+    let owner = contracts.addresses[0];
+    let DAOuser = contracts.addresses[1];
+
+
+    // let proposal = {
+    //   targets: [netEmissionsTokenNetwork.address], // contract to call
+    //   values: [
+    //     owner.address, // address to issue to
+    //     0, // tokenTypeId
+    //     300, // quantity
+    //     0, // fromDate
+    //     0, // thruDate
+    //     0, // automaticRetireDate
+    //     "metadata", // metadata
+    //     "manifest", // manifest
+    //     "description" // description
+    //   ],
+    //   signatures: ["issue"], // function in contract to call
+    //   calldatas: [],
+    //   description: "Test proposal"
+    // };
+
+    let simpleProposal = {
+      targets: [netEmissionsTokenNetwork.address], // contract to call
+      values: [],
+      signatures: ["getNumOfUniqueTokens"], // function in contract to call
+      calldatas: [],
+      description: "Test NetEmissionsTokenNetwork call for getNumOfUniqueTokens"
+    };
+
+    // try making proposal from DAOuser without enough tokens
+    try {
+      let failedProposal = await contracts.governor.connect(DAOuser).propose(
+        simpleProposal.targets,
+        simpleProposal.values,
+        simpleProposal.signatures,
+        simpleProposal.calldatas,
+        simpleProposal.description
+      );
+    } catch (err) {
+      expect(err.toString()).to.equal(
+        "Error: VM Exception while processing transaction: revert GovernorAlpha::propose: proposer votes below proposal threshold"
+      );
+    }
+
+    // send some DAO tokens from owner to DAOuser
+    let transferTokensFromOwnerToDaoUser = await contracts.daoToken.connect(owner).transfer(DAOuser.address, 100000000000000);
+    expect(transferTokensFromOwnerToDaoUser);
+    let transferTokensFromOwnerToDaoUserReceipt = await transferTokensFromOwnerToDaoUser.wait(1);
+
+    // let makeProposal = await contracts.governor.connect(owner).propose(
+    //   simpleProposal.targets,
+    //   simpleProposal.values,
+    //   simpleProposal.signatures,
+    //   simpleProposal.calldatas,
+    //   simpleProposal.description
+    // );
 
   });
 });
