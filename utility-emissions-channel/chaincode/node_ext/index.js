116dbc7c6f11c2c09c0fd2e09d97d0f4cdf1a12c
Konstantin Rybalko <konstantin.rybalko@gmail.com>
2021-03-07 16:25:09 +0200

    Fix response format
    Signed-off-by: Konstantin Rybalko <konstantin.rybalko@gmail.com>

--- utility-emissions-channel/chaincode/node_ext/index.js
+++ utility-emissions-channel/chaincode/node_ext/index.js
@@ -59,7 +59,7 @@ class EmissionsChaincode extends shim.ChaincodeInterface  {
         fieldsMap.url,
         fieldsMap.md5);
 
-    return emissionsRecord;
+    return Buffer.from(JSON.stringify(emissionsRecord));
   }
 
   async updateEmissionsRecord(stub, args, thisClass) {
@@ -115,7 +115,7 @@ class EmissionsChaincode extends shim.ChaincodeInterface  {
         fieldsMap.md5,
         fieldsMap.tokenId);
 
-    return emissionsRecord;
+    return Buffer.from(JSON.stringify(emissionsRecord));
   }
 
   async getEmissionsData(stub, args, thisClass) {
@@ -127,7 +127,7 @@ class EmissionsChaincode extends shim.ChaincodeInterface  {
     let erc = new EmissionsRecordContract(stub);
     let emissionsRecord = await erc.getEmissionsData(args[0]);
 
-    return emissionsRecord;
+    return Buffer.from(emissionsRecord);
   }
 
   async getAllEmissionsData(stub, args, thisClass) {
@@ -141,7 +141,7 @@ class EmissionsChaincode extends shim.ChaincodeInterface  {
     let erc = new EmissionsRecordContract(stub);
     let emissionsRecord = await erc.getAllEmissionsData(args[0], args[1]);
 
-    return emissionsRecord;
+    return Buffer.from(emissionsRecord);
   }
 
   async getAllEmissionsDataByDateRange(stub, args, thisClass) {
@@ -155,7 +155,7 @@ class EmissionsChaincode extends shim.ChaincodeInterface  {
     let erc = new EmissionsRecordContract(stub);
     let emissionsRecord = await erc.getAllEmissionsDataByDateRange(args[0], args[1]);
 
-    return emissionsRecord;
+    return Buffer.from(emissionsRecord);
   }
 
   async getAllEmissionsDataByDateRangeAndParty(stub, args, thisClass) {
@@ -169,7 +169,7 @@ class EmissionsChaincode extends shim.ChaincodeInterface  {
     let erc = new EmissionsRecordContract(stub);
     let emissionsRecord = await erc.getAllEmissionsDataByDateRangeAndParty(args[0], args[1], args[1]);
 
-    return emissionsRecord;
+    return Buffer.from(emissionsRecord);
   }
 
   async importUtilityFactor(stub, args, thisClass) {
@@ -229,7 +229,7 @@ class EmissionsChaincode extends shim.ChaincodeInterface  {
         fieldsMap.renewables,
         fieldsMap.percent_of_renewables);
 
-    return utilityFactor;
+    return Buffer.from(JSON.stringify(utilityFactor));
   }
 
   async updateUtilityFactor(stub, args, thisClass) {
@@ -289,7 +289,7 @@ class EmissionsChaincode extends shim.ChaincodeInterface  {
         fieldsMap.renewables,
         fieldsMap.percent_of_renewables);
 
-    return utilityFactor;
+    return Buffer.from(JSON.stringify(utilityFactor));
   }
 
   async getUtilityFactor(stub, args, thisClass) {
@@ -301,7 +301,7 @@ class EmissionsChaincode extends shim.ChaincodeInterface  {
     let erc = new EmissionsRecordContract(stub);
     let utilityFactor = await erc.getUtilityFactor(args[0]);
 
-    return utilityFactor;
+    return Buffer.from(utilityFactor);
   }
 
   async importUtilityIdentifier(stub, args, thisClass) {
@@ -333,7 +333,7 @@ class EmissionsChaincode extends shim.ChaincodeInterface  {
         fieldsMap.state_province,
         fieldsMap.divisions);
 
-    return utilityIdentifier;
+    return Buffer.from(JSON.stringify(utilityIdentifier));
   }
 
   async updateUtilityIdentifier(stub, args, thisClass) {
@@ -365,7 +365,7 @@ class EmissionsChaincode extends shim.ChaincodeInterface  {
         fieldsMap.state_province,
         fieldsMap.divisions);
 
-    return utilityIdentifier;
+    return Buffer.from(JSON.stringify(utilityIdentifier));
   }
 
   async getUtilityIdentifier(stub, args, thisClass) {
@@ -377,7 +377,7 @@ class EmissionsChaincode extends shim.ChaincodeInterface  {
     let erc = new EmissionsRecordContract(stub);
     let utilityIdentifier = await erc.getUtilityIdentifier(args[0]);
 
-    return utilityIdentifier;
+    return Buffer.from(utilityIdentifier);
   }
 
   async getAllUtilityIdentifiers(stub, args, thisClass) {
@@ -386,7 +386,7 @@ class EmissionsChaincode extends shim.ChaincodeInterface  {
     let erc = new EmissionsRecordContract(stub);
     let utilityIdentifiers = await erc.getAllUtilityIdentifiers();
 
-    return utilityIdentifiers;
+    return Buffer.from(utilityIdentifiers);
   }
 
 }
