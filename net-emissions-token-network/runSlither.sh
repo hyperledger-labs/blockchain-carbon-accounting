0baad74d89bcb5ef505c6f99daaef6a8662b1581
Anton Yip <Anton_Yip@hotmail.com>
2021-03-12 10:23:23 +0800

    Remove truffle stuffs and filters. Its a blacklist
    Signed-off-by: Anton Yip <Anton_Yip@hotmail.com>

--- net-emissions-token-network/runSlither.sh
+++ net-emissions-token-network/runSlither.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+# Dependancies
+# python 3.6+
+# pip3
+# pip3 install slither-analyzer or https://github.com/crytic/slither
+npm install
 pip3 install slither-analyzer
 rm -f slitherlog.json
 slither . --config-file slither.config.json
