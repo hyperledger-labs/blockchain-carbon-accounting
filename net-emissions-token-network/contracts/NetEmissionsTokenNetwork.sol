d34477253e69a5d475517b1fd75858cf9b1847bf
Jameson Hodge <contact@jamesonhodge.com>
2021-03-11 21:13:13 -0500

    net-emissions-token-network - update .gitignore, remove random text
    Signed-off-by: Jameson Hodge <contact@jamesonhodge.com>

--- net-emissions-token-network/contracts/NetEmissionsTokenNetwork.sol
+++ net-emissions-token-network/contracts/NetEmissionsTokenNetwork.sol
@@ -514,7 +514,6 @@ contract NetEmissionsTokenNetwork is Initializable, ERC1155Upgradeable, AccessCo
 
     /**
      * @dev returns issuer of a given tokenId
-repository into your local repo. Fetching is what you do when you want to see what everybody else has been working on. It’s similar to svn update in that it lets you see how the central history has progressed, but it doesn’t force you to actually merge the changes into your repository. Git isolates fetched content from existing local content; it has absolutely no effect on your local     * @param tokenId token to check
      */
     function getIssuer(uint256 tokenId)
         external
