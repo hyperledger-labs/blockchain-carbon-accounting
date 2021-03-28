70449a54aa965392ddf0c74fdd65b4d7edad1348
Zzocker <pkspritam16@gmail.com>
2021-03-29 00:13:29 +0530

    typo fix in startExtChaincode script
    Signed-off-by: Zzocker <pkspritam16@gmail.com>

--- utility-emissions-channel/docker-compose-setup/scripts/startExtChaincode.sh
+++ utility-emissions-channel/docker-compose-setup/scripts/startExtChaincode.sh
@@ -2,5 +2,5 @@
 NETWORK_NAME="carbonAccounting"
 
 docker-compose -f ./docker/nodes/node-one/docker-compose-chaincode.yaml -f ./docker/nodes/node-two/docker-compose-chaincode.yaml up -d
-docker network connect $NETWORK_NAME chaincode_utilityemissions1
-docker network connect $NETWORK_NAME chaincode_utilityemissions2
+docker network connect $NETWORK_NAME chaincode-utilityemissions1.carbonAccounting.com
+docker network connect $NETWORK_NAME chaincode-utilityemissions2.carbonAccounting.com
