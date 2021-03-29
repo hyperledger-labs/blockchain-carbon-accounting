ca751d9c995cc6381d4c98ca63652b166b2dbb09
Konstantin Rybalko <konstantin.rybalko@gmail.com>
2021-03-29 18:09:06 +0300

    Scripts to deploy chaincode ext
    Signed-off-by: Konstantin Rybalko <konstantin.rybalko@gmail.com>

--- utility-emissions-channel/docker-compose-setup/scripts/installChaincode.sh
+++ utility-emissions-channel/docker-compose-setup/scripts/installChaincode.sh
@@ -1,16 +1,14 @@
 CHANNEL_NAME="utilityemissionchannel"
 # CC_NAME="emissionscontract"
-# CC_SRC_PATH=${3:-"NA"}
-# CC_SRC_LANGUAGE=${4:-"javascript"}
-# CC_VERSION=${5:-"1.0"}
-# CC_SEQUENCE=${6:-"1"}
-# CC_INIT_FCN=${7:-"NA"}
-# CC_END_POLICY=${8:-"NA"}
-# CC_COLL_CONFIG=${9:-"NA"}
-# DELAY=${10:-"3"}
-# MAX_RETRY=${11:-"5"}
-# VERBOSE=${12:-"false"}
 LOG_FILE_NAME=chaincode${2}_log.txt
+CC_SUBDIR="one"
+NODE_SUBDIR="node-one"
+CC_NN=${2}
+
+if [ $CC_NN -eq 2 ]; then
+  CC_SUBDIR="two"
+  NODE_SUBDIR="node-two"
+fi
 
 export FABRIC_CFG_PATH=$PWD/fabric-config/
 export PATH=${PWD}/bin:$PATH
@@ -32,16 +30,18 @@ cat log.txt
 echo "===================== Query chaincode on $PEERS on channel '$CHANNEL_NAME' ===================== "
 echo
 
-cd ./chaincode/one
+cd ./chaincode/${CC_SUBDIR}
 # tar connection.json and metadata.json
 tar cfz code.tar.gz connection.json
 tar cfz utilityemissions-chaincode.tgz code.tar.gz metadata.json
 
 cd ../..
 
-./bin/peer lifecycle chaincode install chaincode/one/utilityemissions-chaincode.tgz >&$LOG_FILE_NAME
+./bin/peer lifecycle chaincode install chaincode/${CC_SUBDIR}/utilityemissions-chaincode.tgz >&$LOG_FILE_NAME
 
-CHAINCODE_CCID=`cat ${LOG_FILE_NAME} | grep "Chaincode code package identifier:" | awk '{split($0,a,"Chaincode code package identifier:"); print a[2]}'`
+export CHAINCODE_CCID=`cat ${LOG_FILE_NAME} | grep "Chaincode code package identifier:" | awk '{split($0,a,"Chaincode code package identifier:"); print a[2]}'`
+
+sed -i -e "s!CHAINCODE_CCID=.*!CHAINCODE_CCID=${CHAINCODE_CCID}!g" docker/nodes/${NODE_SUBDIR}/docker-compose-chaincode.yaml
 
 ### Examples
 # sudo bash ./scripts/installChaincode.sh 1 2
