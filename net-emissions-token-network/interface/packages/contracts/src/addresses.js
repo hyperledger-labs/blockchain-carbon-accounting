ef229197551d7d908bd9814643c2dd38c57ed1d5
Jameson Hodge <contact@jamesonhodge.com>
2021-04-15 13:23:11 -0400

    deploy script: separate out clm8 token from dao
    - update default hardhat addresses in interface since the new script changes them
    
    Signed-off-by: Jameson Hodge <contact@jamesonhodge.com>

--- net-emissions-token-network/interface/packages/contracts/src/addresses.js
+++ net-emissions-token-network/interface/packages/contracts/src/addresses.js
@@ -10,10 +10,10 @@ const networksAndAddresses = {
     },
     dao: {
       governor: {
-        address: "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0", // default Hardhat Network deployment address with npx hardhat node
+        address: "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9", // default Hardhat Network deployment address with npx hardhat node
       },
       daoToken: {
-        address: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512", // default Hardhat Network deployment address with npx hardhat node
+        address: "0x5FbDB2315678afecb367f032d93F642f64180aa3", // default Hardhat Network deployment address with npx hardhat node
       }
     }
   },
@@ -34,7 +34,7 @@ const networksAndAddresses = {
   },
 
   xdai: {
-    network: "Xdai Mainet",
+    network: "xDai Mainnet",
     tokenNetwork: {
       address: "0xf7B361Fe2dE41Cd092D2c777D2136CB8E3C2B146",   
     },
