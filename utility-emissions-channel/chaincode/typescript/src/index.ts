66b526726bca8b75777eef73811e1e13a24ddb6d
Konstantin Rybalko <konstantin.rybalko@gmail.com>
2021-06-01 16:52:46 +0300

    RM163 Fix update emissions record
    Signed-off-by: Konstantin Rybalko <konstantin.rybalko@gmail.com>
    Signed-off-by: Pihu1998 <pranamika.pihu1998@gmail.com>

--- utility-emissions-channel/chaincode/typescript/src/index.ts
+++ utility-emissions-channel/chaincode/typescript/src/index.ts
@@ -96,10 +96,15 @@ class EmissionsChaincode {
 
   async updateEmissionsRecord(stub:ChaincodeStub,args:string[]):Promise<ChaincodeResponse>{
     const fields = ['uuid', 'utilityId', 'partyId', 'fromDate','thruDate','emissionsAmount','renewableEnergyUseAmount','nonrenewableEnergyUseAmount','energyUseUom','factorSource','url','md5','tokenId'];
+    const numberFields = ['emissionsAmount','renewableEnergyUseAmount','nonrenewableEnergyUseAmount']
     const recordI:EmissionsRecordInterface = {};
     const fieldsLen = Math.min(args.length, fields.length);
     for (let i=0; i<fieldsLen; i++) {
-        recordI[fields[i]] = args[i];
+        if (numberFields.indexOf(fields[i]) > -1) {
+            recordI[fields[i]] = Number(args[i]);
+        } else {
+            recordI[fields[i]] = args[i];
+        }
     }
     let byte:Uint8Array;
     try {
