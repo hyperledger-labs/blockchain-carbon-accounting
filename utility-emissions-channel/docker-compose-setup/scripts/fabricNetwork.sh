bcdabc4189460b687184d35ae85a94a50fb02f26
Pritam Singh <pkspritam16@gmail.com>
2021-07-31 20:53:02 +0530

    script: added reset command to  fabricNetwork.sh
    Signed-off-by: Pritam Singh <pkspritam16@gmail.com>

--- utility-emissions-channel/docker-compose-setup/scripts/fabricNetwork.sh
+++ utility-emissions-channel/docker-compose-setup/scripts/fabricNetwork.sh
@@ -13,6 +13,40 @@ case $CMD in
         DOCKERE_CMD=restart
         ./scripts/startCli.sh
     ;;
+    "reset")
+        DOCKERE_CMD="down --volumes --remove-orphans"
+
+        # remove channel artifacts
+        rm -r channel-artifacts/*
+        # remove txt files
+        rm -r chaincode1_log.txt chaincode2_log.txt log.txt
+        # remove crypto files
+        rm -r organizations/peerOrganizations
+        cd organizations/fabric-ca
+        cd auditor1
+        rm -r msp ca-cert.pem fabric-ca-server.db IssuerPublicKey IssuerRevocationPublicKey tls-cert.pem
+        cd ../auditor2
+        rm -r msp ca-cert.pem fabric-ca-server.db IssuerPublicKey IssuerRevocationPublicKey tls-cert.pem
+        cd ../../..
+
+        # clean containers
+        if [ "$(docker ps -q -f name=api)" ]; then
+            docker rm -f api
+        fi
+
+        if [ "$(docker ps -q -f name=vault)" ]; then
+            docker rm -f vault
+        fi
+
+        if [ "$(docker ps -q -f name=locals3)" ]; then
+            docker rm -f locals3
+        fi
+
+        if [ "$(docker ps -q -f name=cli)" ]; then
+            docker rm -f cli
+        fi
+    ;;
+
     *)
         echo "command $CMD not supported"
         exit 1
