92a65530141fee5ba6389aca25a1c1caaf19e396
Jeremy Wickersheimer <jwickers@gmail.com>
2022-02-08 12:22:08 +0800

    Add .env file for the credentials, pass tracking number as parameter
    Signed-off-by: Jeremy Wickersheimer <jwickers@gmail.com>

--- supply-chain/ups.js
+++ supply-chain/ups.js
@@ -1,15 +1,26 @@
 // npm install ups-nodejs-sdk
 var upsAPI = require('ups-nodejs-sdk');
+require('dotenv').config();
 
-var ups = new upsAPI({
-  environment: 'live', // sandbox or live
-  username: 'xxxxxxx', 
-  password: 'xxxxxxx',
-  access_key: 'xxxxxxxxxxxxxxxx', // 
-  imperial: true // set to false for metric
-});
+const args = process.argv.slice(2);
+if (args.length != 1) {
+  console.error('The tracking number argument is required!');
+  return 1;
+}
+const trackingNumber = args[0];
 
-ups.track('ups-tracking-number', {latest: false}, (err, res) => {
-  console.log('err? ', err);
-  console.log('res? ', JSON.stringify(res, null, 4));
+const conf = {
+  environment: process.env.UPS_ENV,
+  username: process.env.UPS_USER,
+  password: process.env.UPS_PASSWORD,
+  access_key: process.env.UPS_KEY,
+  imperial: 'true' === process.env.UPS_IMPERIAL
+}
+console.log(conf);
+
+var ups = new upsAPI(conf);
+
+ups.track(trackingNumber, {latest: false}, (err, res) => {
+  if (err) console.error('An error occurred: ', err);
+  else console.log(JSON.stringify(res, null, 4));
 })
