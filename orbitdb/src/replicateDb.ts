e5bf58b90f04d4fdb832a1be90c31fa0c26346c6
Jeremy Wickersheimer <jwickers@gmail.com>
2022-03-07 15:31:12 +0800

    Add more options
    Signed-off-by: Jeremy Wickersheimer <jwickers@gmail.com>

--- orbitdb/src/replicateDb.ts
+++ orbitdb/src/replicateDb.ts
@@ -1,32 +1,65 @@
 import IPFS = require('ipfs')
+import { create } from 'ipfs-http-client';
 import type { OrbitDB as ODB } from 'orbit-db'
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const OrbitDB = require('orbit-db')
 import { SingleBar, Presets } from 'cli-progress';
-import { orbitDbFullPath, orbitDbDirectory, ipfsOptions } from './config'
+import { orbitDbFullPath, orbitDbDirectory, ipfsOptions, ipfsRemoteNode } from './config'
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
+
+
+let useHttpClient = false;
 
 async function main()
 {
   const argv = yargs(hideBin(process.argv))
     .option('serve', {
-      alias: 's',
       type: 'boolean',
       description: 'Keep the node running',
     })
+    .option('bootstrap', {
+      type: 'string',
+      description: 'Set the custom bootstrap node address, default: '+ipfsRemoteNode,
+    })
+    .option('ipfsapi', {
+      type: 'string',
+      description: 'Connect to this IPFS API endpoint instead of running an IPFS node, eg: http://127.0.0.1:5001/api/v0',
+    })
+    .option('ipfsdir', {
+      type: 'string',
+      description: 'Directory of the IPFS data',
+    })
+    .option('orbitdir', {
+      type: 'string',
+      description: 'Directory of the orbit DB',
+    })
     .recommendCommands()
     .showHelpOnFail(true).argv;
+  if (argv['http']) {
+    useHttpClient = true;
+  }
+  if (argv['ipfsdir']) {
+    ipfsOptions.repo = argv['ipfsdir']
+  }
+  if (argv['bootstrap']) {
+    ipfsOptions.config.Bootstrap = [argv['bootstrap']]
+  }
 
   // Create IPFS instance
-  console.log('=== Starting IPFS')
-  const ipfs = await IPFS.create(ipfsOptions)
+  if (useHttpClient) {
+    console.log(`=== Connecting to IPFS ${argv['http']}`)
+  } else {
+    console.log('=== Starting IPFS')
+  }
+  const ipfs = useHttpClient ? create({url: argv['http']}) : await IPFS.create(ipfsOptions)
 
   // Create OrbitDB
-  console.log('=== Starting OrbitDB')
-  const orbitdb: ODB = await OrbitDB.createInstance(ipfs,{directory: orbitDbDirectory})
+  const orbitDir = argv['orbitdir'] || orbitDbDirectory
+  console.log('=== Starting OrbitDB using directory: ', orbitDir)
+  const orbitdb: ODB = await OrbitDB.createInstance(ipfs,{directory: orbitDir})
   const db = await orbitdb.docstore(orbitDbFullPath)
-  const progressBar = new SingleBar(
+  const replicationBar = new SingleBar(
     {
       format:
       'Replicating into OrbitDB |' +
@@ -40,48 +73,86 @@ async function main()
     },
     Presets.shades_classic,
   )
-  let barStarted = false;
+const loadingBar = new SingleBar(
+    {
+      format:
+      'Loading OrbitDB |' +
+        '{bar}' +
+        '| {percentage}% | ETA: {eta}s | {value}/{total} records',
+      barCompleteChar: '\u2588',
+      barIncompleteChar: '\u2591',
+      hideCursor: true,
+      etaBuffer: 50,
+      etaAsynchronousUpdate: true,
+    },
+    Presets.shades_classic,
+  )
+  let replicationBarStarted = false;
+  let loadingBarStarted = false;
+
+  const stopBars = () => {
+    if (replicationBarStarted) replicationBar.stop();
+    if (loadingBarStarted) loadingBar.stop();
+  }
 
   const done = async () => {
-    if (barStarted) progressBar.stop();
+    stopBars();
     console.log('Done');
-    if (!barStarted) console.log('=== OrbitDB was already up-to-date.')
+    if (!replicationBarStarted) console.log('=== OrbitDB was already up-to-date.')
     else console.log('=== OrbitDB replication complete.')
 
-    if (argv['s']) {
+    if (argv['serve']) {
       console.log('OrbitDB Started ... (press CTRL-C to stop)')
     } else {
       console.log('=== Closing OrbitDB ...')
       await db.close()
-      console.log('=== Stopping IPFS, or press CTRL-C to terminate. ...')
-      try {
-        await ipfs.stop()
-      } catch (err) {
-        console.error('== Error stopping IPFS, should not matter.', err)
+      if (!useHttpClient) {
+        console.log('=== Stopping IPFS, or press CTRL-C to terminate. ...')
+        try {
+          await ipfs.stop()
+        } catch (err) {
+          console.error('== Error stopping IPFS, should not matter.', err)
+        }
+        console.log('=== All stopped')
+      } else {
+        console.log('=== All stopped, press CTRL-C to terminate. ...')
       }
-      console.log('=== All stopped')
     }
-
   }
 
   console.log('=== Making existing OrbitDB replica')
-  db.events.on('ready', (dbname) => {
-    console.log('Connected to OrbitDB: ', dbname)
+  db.events.on('load.progress', async (_address, _hash, _entry, progress, have) => {
+    if (!loadingBarStarted) {
+      console.log('Loading DB...')
+      loadingBar.start(have, progress)
+      loadingBarStarted = true
+    } else {
+      loadingBar.update(progress)
+    }
+  });
+  db.events.on('ready', async () => {
+    stopBars()
+    console.log('OrbitDB ready')
+    const loadedRes = db.get('')
+    console.log(`Current number of records: ${loadedRes.length}`)
+    done()
   });
   db.events.on('load', async (dbname) => {
-    console.log('Loaded OrbitDB: ', dbname)
-    done();
+    console.log('Loading OrbitDB: ', dbname)
   });
   db.events.on('replicate.progress', (_address, _hash, _entry, progress, have) => {
-    if (!barStarted) {
+    if (!replicationBarStarted) {
       console.log('Starting replication...')
-      progressBar.start(have, progress)
-      barStarted = true
+      replicationBar.start(have, progress)
+      replicationBarStarted = true
     } else {
-      progressBar.update(progress)
+      replicationBar.update(progress)
     }
   });
   db.events.on('replicated', () => {
+    stopBars()
+    const loadedRes = db.get('')
+    console.log(`Replicated number of records: ${loadedRes.length}`)
     done();
   });
   await db.load()
