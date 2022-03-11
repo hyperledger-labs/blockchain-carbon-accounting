9364eadda61d308caac074da08e77767d2aee14f
Jeremy Wickersheimer <jwickers@gmail.com>
2022-03-11 10:30:16 +0800

    Change ts-node to esr (esbuild-runner) to fix a weird bug with yargs argument parsing
    Signed-off-by: Jeremy Wickersheimer <jwickers@gmail.com>

--- orbitdb/README.md
+++ orbitdb/README.md
@@ -11,7 +11,7 @@ Run `npm install`.
 To replicate the data from the upstream server:
 
 ```
-npx ts-node src/replicateDb.ts --orbitaddress zdpuAwsVJEKAebXSPJj75UimncxdD11RJ1BbbRPYYVrVdCjed --bootstrap /ip4/52.204.157.187/tcp/4001/p2p/12D3KooWPKZ2NrS2wGxCQLV2DsEtdZDNRsPhTzdx18PHNvJDeWrQ
+npx esr src/replicateDb.ts --orbitaddress zdpuAwsVJEKAebXSPJj75UimncxdD11RJ1BbbRPYYVrVdCjed --bootstrap /ip4/52.204.157.187/tcp/4001/p2p/12D3KooWPKZ2NrS2wGxCQLV2DsEtdZDNRsPhTzdx18PHNvJDeWrQ
 ```
 
 By default, this script starts an IPFS node.  You can also use another IPFS node, such as a local node started with `ipfs daemon --enable-pubsub-experiment`.  In that case, make sure you enable `pubsub`.  Then you can use `--ipfsapi local` to point to your local node.
@@ -22,11 +22,11 @@ To keep the Orbit DB process running use the `--serve` flag, this allows using t
 
 This script stores data in `./orbitIpfs` or another directory set by `--ipfsdir` and the orbit DB metadata in `./orbitdb` or another directory set by `--orbitdir`.
 
-Run `npx ts-node src/replicateDb.ts --orbitaddress zdpuAwsVJEKAebXSPJj75UimncxdD11RJ1BbbRPYYVrVdCjed --ipfsapi local --orbitdir orbitdb_2 --serve` to replicate using the local IPFS daemon and keep it running.
+Run `npx esr src/replicateDb.ts --orbitaddress zdpuAwsVJEKAebXSPJj75UimncxdD11RJ1BbbRPYYVrVdCjed --ipfsapi local --orbitdir orbitdb_2 --serve` to replicate using the local IPFS daemon and keep it running.
 
 When this works, close the serving script and you can run:
 ```
-$ npx ts-node src/getData.ts --orbitaddress zdpuAwsVJEKAebXSPJj75UimncxdD11RJ1BbbRPYYVrVdCjed test
+$ npx esr src/getData.ts --orbitaddress zdpuAwsVJEKAebXSPJj75UimncxdD11RJ1BbbRPYYVrVdCjed test
 { emission: { value: 17720, uom: 'kg' }, year: 2021 }
 ```
 
@@ -41,7 +41,7 @@ ipfs id
 
 ```
 
-Then you can run `npx ts-node src/replicateDb.ts --orbitaddress zdpuAwsVJEKAebXSPJj75UimncxdD11RJ1BbbRPYYVrVdCjed --orbitdir orbitdb_3 --bootstrap /ip4/127.0.0.1/tcp/4001/p2p/12D3KooWDGUDi3vMhdp3gSq2DM3hJoXBQmmkVPEPcRHZGGPGqit3` to replicate against the local node instead of the upstream server.
+Then you can run `npx esr src/replicateDb.ts --orbitaddress zdpuAwsVJEKAebXSPJj75UimncxdD11RJ1BbbRPYYVrVdCjed --orbitdir orbitdb_3 --bootstrap /ip4/127.0.0.1/tcp/4001/p2p/12D3KooWDGUDi3vMhdp3gSq2DM3hJoXBQmmkVPEPcRHZGGPGqit3` to replicate against the local node instead of the upstream server.
 
 ## Loading Data
 
@@ -51,7 +51,7 @@ If you want to load your own data, you will need to get your own emissions facto
 Once you download them, use
 
 ```
-$ npx ts-node src/dataLoader.ts load_utility_emissions conversion-factors-2021-flat-file-automatic-processing.xls --orbitcreate
+$ npx esr src/dataLoader.ts load_emissions_factors conversion-factors-2021-flat-file-automatic-processing.xls --orbitcreate
 ```
 
 It will create an orbitdb address, then parse the worksheet, then load into orbitdb.  Then follow the steps from Replicating Data above to get your node id for replicating it.
@@ -62,7 +62,7 @@ It will create an orbitdb address, then parse the worksheet, then load into orbi
 
 Create a DB and load:
 ```
-npx ts-node src/dataLoader.ts load_utility_emissions conversion-factors-2021-flat-file-automatic-processing.xls --orbitcreate
+npx esr src/dataLoader.ts load_utility_emissions conversion-factors-2021-flat-file-automatic-processing.xls --orbitcreate
 ```
 
 Optionally you can specify `--orbitdir orbitdb1 --ipfsdir orbitipfs1` to use different data folders when creating the DB.
@@ -74,22 +74,22 @@ Example output:
 
 The getData script can then be used with (using the same directories parameters if those were given above):
 ```
-npx ts-node src/getData.ts --orbitaddress zdpuAxZtb1Vy92R1LxVQvzuXvuCEv4aLtGrBdM8bV63ciN4s3 test
+npx esr src/getData.ts --orbitaddress zdpuAxZtb1Vy92R1LxVQvzuXvuCEv4aLtGrBdM8bV63ciN4s3 test
 ```
 
 Then you can `serve` it (using the same directories parameters if those were given above) with:
 ```
-npx ts-node src/replicateDb.ts --orbitaddress zdpuAxZtb1Vy92R1LxVQvzuXvuCEv4aLtGrBdM8bV63ciN4s3 --serve
+npx esr src/replicateDb.ts --orbitaddress zdpuAxZtb1Vy92R1LxVQvzuXvuCEv4aLtGrBdM8bV63ciN4s3 --serve
 ```
 
 Replication in another IPFS node, not I added a port parameter since the first node would already use 4001:
 ```
-npx ts-node src/replicateDb.ts --ipfsdir orbitipfs_2 --orbitdir orbitdb_2 --ipfsport 4002 --orbitaddress zdpuAxZtb1Vy92R1LxVQvzuXvuCEv4aLtGrBdM8bV63ciN4s3
+npx esr src/replicateDb.ts --ipfsdir orbitipfs_2 --orbitdir orbitdb_2 --ipfsport 4002 --orbitaddress zdpuAxZtb1Vy92R1LxVQvzuXvuCEv4aLtGrBdM8bV63ciN4s3
 ```
 
 This can also be tested for query:
 ```
-npx ts-node src/getData.ts --orbitaddress zdpuAxZtb1Vy92R1LxVQvzuXvuCEv4aLtGrBdM8bV63ciN4s3 --ipfsdir orbitipfs_2 --orbitdir orbitdb_2 --ipfsport 4002 test
+npx esr src/getData.ts --orbitaddress zdpuAxZtb1Vy92R1LxVQvzuXvuCEv4aLtGrBdM8bV63ciN4s3 --ipfsdir orbitipfs_2 --orbitdir orbitdb_2 --ipfsport 4002 test
 ```
 
 ## Using a local IPFS node
@@ -101,10 +101,10 @@ ipfs daemon --enable-pubsub-experiment
 
 In that case the DB creation can be done with:
 ```
-npx ts-node src/dataLoader.ts load_utility_emissions conversion-factors-2021-flat-file-automatic-processing.xls --orbitcreate --ipfsapi local --orbitdir orbitdb_local
+npx esr src/dataLoader.ts load_utility_emissions conversion-factors-2021-flat-file-automatic-processing.xls --orbitcreate --ipfsapi local --orbitdir orbitdb_local
 ```
 
 Then query (assuming this created zdpuAsPTgBqwm9W3gff2BA4snv4NJ6TVEZpD1DcFjBxaDvx2m) (note: `--ipfsapi local` is a shortcut for `--ipfsapi http://127.0.0.1:5001/api/v0`, use the later form if your IPFS node is setup with a different API port):
 ```
-npx ts-node src/getData.ts --orbitaddress zdpuAsPTgBqwm9W3gff2BA4snv4NJ6TVEZpD1DcFjBxaDvx2m --ipfsapi local --orbitdir orbitdb_local test
+npx esr src/getData.ts --orbitaddress zdpuAsPTgBqwm9W3gff2BA4snv4NJ6TVEZpD1DcFjBxaDvx2m --ipfsapi local --orbitdir orbitdb_local test
 ```
--- orbitdb/src/getData.ts
+++ orbitdb/src/getData.ts
@@ -1,105 +1,124 @@
 import yargs = require('yargs')
-import { hideBin } from "yargs/helpers";
-import { EmissionsFactorInterface } from "../../emissions-data/chaincode/emissionscontract/typescript/src/lib/emissionsFactor";
-import { addCommonYargsOptions } from "./config";
-import { ActivityInterface, OrbitDBService } from "./orbitDbService";
+import { hideBin } from "yargs/helpers"
+import { EmissionsFactorInterface } from "../../emissions-data/chaincode/emissionscontract/typescript/src/lib/emissionsFactor"
+import { addCommonYargsOptions } from "./config"
+import { ActivityInterface, OrbitDBService } from "./orbitDbService"
 
-let db: OrbitDBService;
+let db: OrbitDBService
 
 (async () => {
   const init = async (argv) => {
-    await OrbitDBService.init(argv);
-    db = new OrbitDBService();
-  };
+    await OrbitDBService.init(argv)
+    db = new OrbitDBService()
+  }
   addCommonYargsOptions(yargs(hideBin(process.argv)))
-    .command("test", "Run some test queries", {}, async (args) => {
-      await init(args);
+  .command("test", "Run some test queries", {}, async (args) => {
+    await init(args)
 
-      const activity: ActivityInterface = {
-        scope: "scope 1",
-        level_1: "REFRIGERANT & OTHER",
-        level_2: "KYOTO PROTOCOL - STANDARD",
-        level_3: "PERFLUOROBUTANE (PFC-3-1-10)",
-        activity_uom: "kg",
-        activity: 2,
-      };
-      const factor: EmissionsFactorInterface = {
-        text: "",
-        type: "UTILITY_EMISSIONS_SCOPE_1",
-        uuid: "SCOPE_1 PERFLUOROBUTANE (PFC-3-1-10)",
-        year: "2021",
-        class:
-          "org.hyperledger.blockchain-carbon-accounting.utilityemissionsfactoritem",
-        scope: "Scope 1",
-        source:
-          "https://www.gov.uk/government/publications/greenhouse-gas-reporting-conversion-factors-2021",
-        level_1: "REFRIGERANT & OTHER",
-        level_2: "KYOTO PROTOCOL - STANDARD",
-        level_3: "PERFLUOROBUTANE (PFC-3-1-10)",
-        activity_uom: "kg",
-        co2_equivalent_emissions: "8860",
-        co2_equivalent_emissions_uom: "kg",
-      };
-      console.log("Test getCO2EmissionFactorByActivity ...");
-      console.log(db.getCO2EmissionFactorByActivity(factor, activity));
+    const activity: ActivityInterface = {
+      scope: "scope 1",
+      level_1: "REFRIGERANT & OTHER",
+      level_2: "KYOTO PROTOCOL - STANDARD",
+      level_3: "PERFLUOROBUTANE (PFC-3-1-10)",
+      activity_uom: "kg",
+      activity: 2,
+    }
+    const factor: EmissionsFactorInterface = {
+      text: "",
+      type: "UTILITY_EMISSIONS_SCOPE_1",
+      uuid: "SCOPE_1 PERFLUOROBUTANE (PFC-3-1-10)",
+      year: "2021",
+      class:
+      "org.hyperledger.blockchain-carbon-accounting.utilityemissionsfactoritem",
+      scope: "Scope 1",
+      source:
+      "https://www.gov.uk/government/publications/greenhouse-gas-reporting-conversion-factors-2021",
+      level_1: "REFRIGERANT & OTHER",
+      level_2: "KYOTO PROTOCOL - STANDARD",
+      level_3: "PERFLUOROBUTANE (PFC-3-1-10)",
+      activity_uom: "kg",
+      co2_equivalent_emissions: "8860",
+      co2_equivalent_emissions_uom: "kg",
+    }
 
-      console.log("Test getCO2EmissionByActivity ...");
-      console.log(db.getCO2EmissionByActivity(activity));
-
-      const allLookup = db.getAllFactors();
-      console.log("Test getAllFactors ...");
-      console.log(" got count : ", allLookup ? allLookup.length : null);
+    try {
+      const allLookup = db.getAllFactors()
+      console.log("Test getAllFactors ...")
+      console.log(" got count : ", allLookup ? allLookup.length : null)
       if (allLookup && allLookup.length) {
-        console.log("Sample first factor ...");
-        console.log(allLookup[0]);
+        console.log("Sample first factor ...")
+        console.log(allLookup[0])
       }
-      process.exit(0);
-    })
-    .command("factors", "Query all factors from the DB", {}, async (args) => {
-      await init(args);
-      const allLookup = db.getAllFactors();
-      console.log("count : ", allLookup ? allLookup.length : null);
+    } catch (e) {
+      console.log('Error', e)
+    }
+
+    try {
+      console.log("Test getCO2EmissionFactorByActivity ...")
+      console.log(db.getCO2EmissionFactorByActivity(factor, activity))
+    } catch (e) {
+      console.log('Error', e)
+    }
+
+    try {
+      console.log("Test getCO2EmissionByActivity ...")
+      console.log(db.getCO2EmissionByActivity(activity))
+    } catch (e) {
+      console.log('Error', e)
+    }
+
+    process.exit(0)
+  })
+  .command("factors", "Query all factors from the DB", {}, async (args) => {
+    await init(args)
+    try {
+      const allLookup = db.getAllFactors()
+      console.log("count : ", allLookup ? allLookup.length : null)
 
       if (allLookup && allLookup.length) {
         for (const f of allLookup) {
-          console.log(f);
+          console.log(f)
         }
       }
-      process.exit(0);
-    })
-    .command(
-      "activity-emissions <scope> <level1> <level2> <level3> <amount> [uom]",
-      "Calculate the emissions for an activity",
-      (yargs) => {
-        yargs
-          .positional("scope", {
-            describe: 'The activity scope, eg: "scope 1"',
-            type: "string",
-          })
-          .positional("level1", {
-            describe: "Activity level 1",
-            type: "string",
-          })
-          .positional("level2", {
-            describe: "Activity level 2",
-            type: "string",
-          })
-          .positional("level3", {
-            describe: "Activity level 3",
-            type: "string",
-          })
-          .positional("amount", {
-            describe: "Activity amount",
-            type: "number",
-          })
-          .positional("uom", {
-            describe: 'Activity uom, eg: "kg"',
-            type: "string",
-            default: "kg",
-          });
-      },
-      async (args) => {
-        await init(args);
+    } catch (e) {
+      console.log('Error', e)
+    }
+    process.exit(0)
+  })
+  .command(
+    "activity-emissions <scope> <level1> <level2> <level3> <amount> [uom]",
+    "Calculate the emissions for an activity",
+    (yargs) => {
+      yargs
+        .positional("scope", {
+          describe: 'The activity scope, eg: "scope 1"',
+          type: "string",
+        })
+        .positional("level1", {
+          describe: "Activity level 1",
+          type: "string",
+        })
+        .positional("level2", {
+          describe: "Activity level 2",
+          type: "string",
+        })
+        .positional("level3", {
+          describe: "Activity level 3",
+          type: "string",
+        })
+        .positional("amount", {
+          describe: "Activity amount",
+          type: "number",
+        })
+        .positional("uom", {
+          describe: 'Activity uom, eg: "kg"',
+          type: "string",
+          default: "kg",
+        })
+    },
+    async (args) => {
+      await init(args)
+      try {
         console.log(
           db.getCO2EmissionByActivity({
             scope: args.scope,
@@ -109,12 +128,15 @@ let db: OrbitDBService;
             activity_uom: args.uom,
             activity: args.amount,
           })
-        );
-        process.exit(0);
+        )
+      } catch (e) {
+        console.log('Error', e)
       }
-    )
-    .demandCommand(1)
-    .recommendCommands()
-    .strict()
-    .showHelpOnFail(true).argv;
-})();
+      process.exit(0)
+    }
+  )
+  .demandCommand(1)
+  .recommendCommands()
+  .strict()
+  .showHelpOnFail(true).argv
+})()
