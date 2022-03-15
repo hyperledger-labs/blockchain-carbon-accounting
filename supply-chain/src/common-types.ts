d737c56ee13cfaf8ca4a1ecbbd1112cf1fea53fc
Jeremy Wickersheimer <jwickers@gmail.com>
2022-03-15 12:53:53 +0800

    Add level 4 to supply-chain, change factor desc to text for consistency
    Signed-off-by: Jeremy Wickersheimer <jwickers@gmail.com>

--- supply-chain/src/common-types.ts
+++ supply-chain/src/common-types.ts
@@ -17,7 +17,8 @@ export type EmissionFactor = {
     level_1: string,
     level_2: string,
     level_3: string,
-    level_4: string,
+    level_4?: string,
+    text?: string,
     uom: string,
     amount: number,
 }
