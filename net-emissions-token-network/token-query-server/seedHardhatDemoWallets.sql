9970af5318ab21380123e8afa317b68e3e48f923
Jeremy Wickersheimer <jwickers@gmail.com>
2022-04-18 11:50:09 +0800

    Move Hardhat wallet data seed into a SQL script
    Signed-off-by: Jeremy Wickersheimer <jwickers@gmail.com>

---
+++ net-emissions-token-network/token-query-server/seedHardhatDemoWallets.sql
@@ -0,0 +1,13 @@
+UPDATE wallet SET name = 'super user (Account 0)', organization = 'Test Hardhat' WHERE address = '0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266';
+UPDATE wallet SET name = 'REC Dealer 1', organization = 'Test Hardhat' WHERE address = '0x70997970C51812dc3A010C7d01b50e0d17dc79C8';
+UPDATE wallet SET name = 'Emissions Auditor 1', organization = 'Test Hardhat' WHERE address = '0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC';
+UPDATE wallet SET name = 'Offset Dealer 1', organization = 'Test Hardhat' WHERE address = '0x90F79bf6EB2c4f870365E785982E1f101E93b906';
+UPDATE wallet SET name = 'Emissions Auditor 2', organization = 'Test Hardhat' WHERE address = '0x15d34aaf54267db7d7c367839aaf71a00a2c6a65';
+UPDATE wallet SET name = 'Consumer 1', organization = 'Test Hardhat' WHERE address = '0x8626f6940e2eb28930efb4cef49b2d1f2c9c1199';
+UPDATE wallet SET name = 'Consumer 2', organization = 'Test Hardhat' WHERE address = '0xdD2FD4581271e230360230F9337D5c0430Bf44C0';
+UPDATE wallet SET name = 'Emissions Auditor 3', organization = 'Test Hardhat' WHERE address = '0x9965507D1a55bcC2695C58ba16FB37d819B0A4dc';
+UPDATE wallet SET name = 'Emissions Auditor 4', organization = 'Test Hardhat' WHERE address = '0x976EA74026E726554dB657fA54763abd0C3a0aa9';
+UPDATE wallet SET name = 'Offset Dealer 2', organization = 'Test Hardhat' WHERE address = '0x14dC79964da2C08b23698B3D3cc7Ca32193d9955';
+UPDATE wallet SET name = 'Industry 1', organization = 'Test Hardhat' WHERE address = '0xcd3B766CCDd6AE721141F452C550Ca635964ce71';
+UPDATE wallet SET name = 'Industry 2', organization = 'Test Hardhat' WHERE address = '0x2546BcD3c84621e976D8185a91A922aE77ECEc30';
+
