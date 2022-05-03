d954f18c3c2864ce148e1e1daa52402f124ab16f
Jeremy Wickersheimer <jwickers@gmail.com>
2022-05-03 19:52:45 +0800

    properly link the type dependency on the server api definition
    Signed-off-by: Jeremy Wickersheimer <jwickers@gmail.com>

--- net-emissions-token-network/interface/packages/react-app/src/services/trpc.ts
+++ net-emissions-token-network/interface/packages/react-app/src/services/trpc.ts
@@ -1,6 +1,6 @@
 import { createReactQueryHooks } from '@trpc/react';
 import { useState } from 'react';
-import { type AppRouter } from '../../../../../api-server/trpc/common';
+import type { AppRouter } from 'api-server/trpc/common';
 import { BASE_URL } from './api.config';
 
 export const trpc = createReactQueryHooks<AppRouter>();
