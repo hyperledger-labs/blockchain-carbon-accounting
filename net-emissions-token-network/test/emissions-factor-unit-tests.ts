7e8a4dd44ae0970cc4e22e9191ed8995e2f06b56
Konstantin Rybalko <konstantin.rybalko@gmail.com>
2022-05-12 17:27:41 +0300

    RM622 Automated tests for natural gas, and electricity emissions calculations
    Signed-off-by: Konstantin Rybalko <konstantin.rybalko@gmail.com>

--- net-emissions-token-network/test/emissions-factor-unit-tests.ts
+++ net-emissions-token-network/test/emissions-factor-unit-tests.ts
@@ -2,10 +2,18 @@ import { expect } from 'chai';
 import { v4 as uuidv4 } from 'uuid';
 import { PostgresDBService } from 'blockchain-accounting-data-postgres/src/postgresDbService';
 import type { EmissionsFactorInterface } from 'emissions_data_chaincode/src/lib/emissionsFactor';
+import type { UtilityLookupItemInterface } from "emissions_data_chaincode/src/lib/utilityLookupItem";
+import { process_activity } from 'supply-chain-lib/src/emissions-utils';
+import { Activity } from 'supply-chain-lib/src/common-types';
 
 async function addTestEmissionsFactor(efl: EmissionsFactorInterface) {
   const db = await PostgresDBService.getInstance();
   await db.getEmissionsFactorRepo().putEmissionFactor({...efl});
+}
+
+async function addUtilityLookupItem(uli: UtilityLookupItemInterface) {
+  const db = await PostgresDBService.getInstance();
+  await db.getUtilityLookupItemRepo().putUtilityLookupItem(uli);
 }
 
 async function setupDBSeed() {
@@ -65,6 +73,63 @@ async function setupDBSeed() {
     net_generation_uom: 'MWH',
     co2_equivalent_emissions: '0.2265591892870195',
     co2_equivalent_emissions_uom: 'tons'
+  });
+
+  await addTestEmissionsFactor({
+    uuid: uuidv4(),
+    class: 'org.hyperledger.blockchain-carbon-accounting.emissionsfactoritem',
+    type: 'SCOPE_1_EMISSIONS',
+    scope: 'Scope 1',
+    level_1: 'FUELS',
+    level_2: 'GASEOUS FUELS',
+    level_3: 'NATURAL GAS',
+    level_4: '',
+    text: 'Volume',
+    year: '2019',
+    activity_uom: 'cubic metres',
+    co2_equivalent_emissions: '2.03053',
+    co2_equivalent_emissions_uom: 'kg'
+  });
+  await addTestEmissionsFactor({
+    uuid: uuidv4(),
+    class: 'org.hyperledger.blockchain-carbon-accounting.emissionsfactoritem',
+    type: 'SCOPE_1_EMISSIONS',
+    scope: 'Scope 1',
+    level_1: 'FUELS',
+    level_2: 'GASEOUS FUELS',
+    level_3: 'NATURAL GAS',
+    level_4: '',
+    text: 'Volume',
+    year: '2021',
+    activity_uom: 'cubic metres',
+    co2_equivalent_emissions: '2.02135',
+    co2_equivalent_emissions_uom: 'kg'
+  });
+  await addTestEmissionsFactor({
+    uuid: uuidv4(),
+    class: 'org.hyperledger.blockchain-carbon-accounting.emissionsfactoritem',
+    type: 'SCOPE_1_EMISSIONS',
+    scope: 'Scope 1',
+    level_1: 'FUELS',
+    level_2: 'GASEOUS FUELS',
+    level_3: 'NATURAL GAS',
+    level_4: '',
+    text: 'Volume',
+    year: '2020',
+    activity_uom: 'cubic metres',
+    co2_equivalent_emissions: '2.02266',
+    co2_equivalent_emissions_uom: 'kg'
+  });
+
+  await addUtilityLookupItem({
+    uuid: uuidv4(),
+    class:'org.hyperledger.blockchain-carbon-accounting.utilitylookuplist',
+    year: '2019',
+    utility_number: '14328',
+    utility_name: 'Pacific_Gas_&_Electric_Co.',
+    country: 'USA',
+    state_province: 'CA',
+    divisions: { division_type: 'NERC_REGION', division_id: 'WECC' }
   });
 }
 
@@ -153,5 +218,58 @@ describe("Emissions Factors tests", function() {
     const emissionsFactors = await db.getEmissionsFactorRepo().getEmissionsFactors(condition);
     expect(emissionsFactors).to.have.lengthOf(1);
     expect(emissionsFactors[0]).to.have.property('year').equal('2019');
+  });
+
+  it("should be correct calculation of US electricity based on state", async function() {
+    const db = await PostgresDBService.getInstance();
+    const utilityLookupItems = await db.getUtilityLookupItemRepo().getAllUtilityLookupItems();
+    expect(utilityLookupItems).to.have.lengthOf.at.least(1);
+    const activity: Activity = {
+      id: '1',
+      type: 'electricity',
+      activity_amount: 128,
+      activity_uom: 'kwh',
+      country: 'UNITED STATES',
+      state: 'CA',
+      utility: utilityLookupItems[0].uuid,
+      from_date: new Date('2019-01-31T22:00:00.000Z'),
+      thru_date: new Date('2019-03-01T21:59:59.999Z'),
+      from: '0x8626f6940e2eb28930efb4cef49b2d1f2c9c1199',
+      to: '0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC'
+    }
+    const result = await process_activity(activity);
+    expect(result).to.have.property('emissions');
+    expect(result).to.have.property('emissions').to.have.property('amount');
+    expect(result).to.have.property('emissions').to.have.property('amount').to.have.property('unit').equal('kgCO2e');
+    if (result.emissions && result.emissions.amount && result.emissions.amount.value) {
+      const emissions_amount = result.emissions.amount.value;
+      expect(Math.round(emissions_amount)).equal(25);
+    } else {
+      throw new Error('empty emissions.amount.value');
+    }
+  });
+
+  it("should be correct calculation of natural gas from therm to kgCO2e", async function() {
+    const db = await PostgresDBService.getInstance();
+    const activity: Activity = {
+      id: '1',
+      type: 'natural_gas',
+      activity_amount: 100,
+      activity_uom: 'therm',
+      from_date: new Date('2019-01-31T22:00:00.000Z'),
+      thru_date: new Date('2019-03-01T21:59:59.999Z'),
+      from: '0x8626f6940e2eb28930efb4cef49b2d1f2c9c1199',
+      to: '0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC'
+    }
+    const result = await process_activity(activity);
+    expect(result).to.have.property('emissions');
+    expect(result).to.have.property('emissions').to.have.property('amount');
+    expect(result).to.have.property('emissions').to.have.property('amount').to.have.property('unit').equal('kgCO2e');
+    if (result.emissions && result.emissions.amount && result.emissions.amount.value) {
+      const emissions_amount = result.emissions.amount.value;
+      expect(Math.round(emissions_amount)).equal(575);
+    } else {
+      throw new Error('empty emissions.amount.value');
+    }
   });
 });
