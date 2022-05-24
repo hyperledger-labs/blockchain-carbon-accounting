cc4e18eb99bfc552a7593be8d80b673bccdf0403
Jeremy Wickersheimer <jwickers@gmail.com>
2022-05-24 18:32:12 +0800

    Allow the server to serve requets while synchronizing, temprary out of date data is probably preferable to 500 errors
    Signed-off-by: Jeremy Wickersheimer <jwickers@gmail.com>

--- net-emissions-token-network/api-server/server.ts
+++ net-emissions-token-network/api-server/server.ts
@@ -95,18 +95,30 @@ app.use('/trpc', trpcMiddleware);
  */
 db.then(async () => {
 
-  const lastBlock = await startupSync(OPTS);
+  async function sync() {
+    const lastBlock = await startupSync(OPTS);
 
-  try {
-    // for blockchains that support events subscriptions
-    if (network_name === 'bsctestnet') {
-      console.log('Subscribing to events starting from block:', lastBlock);
-      subscribeToEvents(OPTS);
+    try {
+      // for blockchains that support events subscriptions
+      if (network_name === 'bsctestnet') {
+        console.log('Subscribing to events starting from block:', lastBlock);
+        subscribeToEvents(OPTS);
+      }
+    } catch (err) {
+      console.error('An error occurred while setting up the blockchain event handlers', err);
+      throw err;
     }
-  } catch (err) {
-    console.error('An error occurred while setting up the blockchain event handlers', err)
-    throw err
   }
+  // call this without await so the server can sync but also serve other requests in the meantime
+  // unless we are on hardhat where to would conflict with the auto sync middleware
+  // this means we do not throw 500 errors while this is still loading but the tokens
+  // will show up with out of date or 0 balances until they are done loading
+  if (network_name !== 'hardhat') {
+    sync();
+  } else {
+    await sync();
+  }
+
   app.listen(Number(PORT), '0.0.0.0', () => {
     console.log(`Server is listening on ${PORT}\n`)
   });
