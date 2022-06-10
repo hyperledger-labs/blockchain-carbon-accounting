1046dd420dd72b3c2edd09f6af161106ba8334f6
Jeremy Wickersheimer <jwickers@gmail.com>
2022-06-10 12:21:48 +0800

    Add more debug and abort on error (so it is easier to debug those)
    Signed-off-by: Jeremy Wickersheimer <jwickers@gmail.com>

--- emissions-data/docker-compose-setup/startDev.sh
+++ emissions-data/docker-compose-setup/startDev.sh
@@ -1,32 +1,35 @@
 #!/bin/bash
-echo "Starting blockchain-carbon-accounting repo"
+# exit when any command fails
+set -e
 
-echo "Checking for carbonAccounting network..."
+echo "=== [startDev] Starting blockchain-carbon-accounting repo"
+
+echo "=== [startDev] Checking for carbonAccounting network..."
 ./scripts/startNetwork.sh
 
-echo "Starting CAs..."
+echo "=== [startDev] Starting CAs..."
 ./scripts/startCA.sh
 
-echo "Building chaincode compilation environment..."
+echo "=== [startDev] Building chaincode compilation environment..."
 docker build -f "./docker/golang-node.dockerfile" -t golang-node:latest "./docker"
 
-echo "Starting client..."
+echo "=== [startDev] Starting client..."
 ./scripts/startCli.sh
 
-echo "Generating crpyto and orgs..."
+echo "=== [startDev] Generating crpyto and orgs..."
 docker exec cli /bin/bash ./network.sh up
 
-echo "Starting orderers, peers, and couchdb..."
+echo "=== [startDev] Starting orderers, peers, and couchdb..."
 sh ./scripts/startAndConnectNetwork.sh
 
-echo "Creating the channel..."
+echo "=== [startDev] Creating the channel..."
 docker exec cli /bin/bash ./network.sh createChannel
 
-echo "Installing utility emissions channel TypeScript chaincode..."
+echo "=== [startDev] Installing utility emissions channel TypeScript chaincode..."
 docker exec cli /bin/bash ./network.sh deployCC -ccn emissions -ccp ../chaincode/emissionscontract/typescript -ccv 1 -ccl typescript
 
-echo "Installing datalock chaincode..."
+echo "=== [startDev] Installing datalock chaincode..."
 docker exec cli /bin/bash ./network.sh deployCC -ccn datalock -ccp ../chaincode/datalock -ccv 1 -ccl go
 
-echo "Starting the api..."
+echo "=== [startDev] Starting the api..."
 ./scripts/startApi.sh $1
