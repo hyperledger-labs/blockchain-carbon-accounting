ae867fbabacd58f5289f00774158af8592ae6def
Jeremy Wickersheimer <jwickers@gmail.com>
2022-06-10 10:04:58 +0800

    Avoid calling network connect if already connected
    Signed-off-by: Jeremy Wickersheimer <jwickers@gmail.com>

--- emissions-data/docker-compose-setup/scripts/startAndConnectNetwork.sh
+++ emissions-data/docker-compose-setup/scripts/startAndConnectNetwork.sh
@@ -4,10 +4,10 @@ NETWORK_NAME="carbonAccounting"
 # Start both nodes for development
 docker-compose -f ./docker/nodes/node-one/docker-compose-couch.yaml -f ./docker/nodes/node-one/docker-compose-carbonAccounting.yaml -f ./docker/nodes/node-two/docker-compose-couch.yaml -f ./docker/nodes/node-two/docker-compose-carbonAccounting.yaml up -d
 
-docker network connect $NETWORK_NAME orderer1.auditor1.carbonAccounting.com
-docker network connect $NETWORK_NAME peer1.auditor1.carbonAccounting.com
-docker network connect $NETWORK_NAME couchdb0
+docker network inspect $NETWORK_NAME | grep --silent '"orderer1.auditor1.carbonAccounting.com"' || docker network connect $NETWORK_NAME orderer1.auditor1.carbonAccounting.com
+docker network inspect $NETWORK_NAME | grep --silent '"peer1.auditor1.carbonAccounting.com"' || docker network connect $NETWORK_NAME peer1.auditor1.carbonAccounting.com
+docker network inspect $NETWORK_NAME | grep --silent '"couchdb0"' || docker network connect $NETWORK_NAME couchdb0
 
-docker network connect $NETWORK_NAME orderer1.auditor2.carbonAccounting.com
-docker network connect $NETWORK_NAME peer1.auditor2.carbonAccounting.com
-docker network connect $NETWORK_NAME couchdb1
+docker network inspect $NETWORK_NAME | grep --silent '"orderer1.auditor2.carbonAccounting.com"' || docker network connect $NETWORK_NAME orderer1.auditor2.carbonAccounting.com
+docker network inspect $NETWORK_NAME | grep --silent '"peer1.auditor2.carbonAccounting.com"' || docker network connect $NETWORK_NAME peer1.auditor2.carbonAccounting.com
+docker network inspect $NETWORK_NAME | grep --silent '"couchdb1"' || docker network connect $NETWORK_NAME couchdb1
--- emissions-data/docker-compose-setup/scripts/startCA.sh
+++ emissions-data/docker-compose-setup/scripts/startCA.sh
@@ -2,8 +2,8 @@
 NETWORK_NAME="carbonAccounting"
 
 docker-compose -f ./docker/nodes/node-one/docker-compose-ca.yaml -f ./docker/nodes/node-two/docker-compose-ca.yaml up -d
-docker network connect $NETWORK_NAME auditor1.carbonAccounting.com
-docker network connect $NETWORK_NAME auditor2.carbonAccounting.com
+docker network inspect $NETWORK_NAME | grep --silent '"auditor1.carbonAccounting.com"' || docker network connect $NETWORK_NAME auditor1.carbonAccounting.com
+docker network inspect $NETWORK_NAME | grep --silent '"auditor2.carbonAccounting.com"' || docker network connect $NETWORK_NAME auditor2.carbonAccounting.com
 
 
 # Production settings
