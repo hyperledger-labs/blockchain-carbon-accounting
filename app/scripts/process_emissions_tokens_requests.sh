2854b928423e353de13f28cb07c6659989e7d4eb
Konstantin Rybalko <konstantin.rybalko@gmail.com>
2022-06-28 15:26:13 +0300

    RM677 Fix process emissions requests script
    Signed-off-by: Konstantin Rybalko <konstantin.rybalko@gmail.com>

--- app/scripts/process_emissions_tokens_requests.sh
+++ app/scripts/process_emissions_tokens_requests.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+LOGFILE="/tmp/process_emissions_tokens_requests.log"
+
 cd /home/opentaps/blockchain-carbon-accounting || exit 1
-npm run supply-chain:cli -- -processrequests
+source /home/opentaps/.bashrc
+npm run supply-chain:cli -- -processrequests >> ${LOGFILE}  2>&1
