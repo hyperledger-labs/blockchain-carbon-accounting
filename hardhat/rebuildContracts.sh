5747861727ea0fe6864c5c149181cb5e79aba345
Jeremy Wickersheimer <jwickers@gmail.com>
2022-07-29 11:05:48 +0800

    Fix rebuildContracts script
    Signed-off-by: Jeremy Wickersheimer <jwickers@gmail.com>

--- hardhat/rebuildContracts.sh
+++ hardhat/rebuildContracts.sh
@@ -5,9 +5,9 @@ npx hardhat clean
 echo "Compile ..."
 npx hardhat compile
 echo "Copy ABIs to interface contracts ..."
-cp -fv artifacts/contracts/Governance/DAOToken.sol/DAOToken.json ../app/frontend/contracts/src/abis/
-cp -fv artifacts/contracts/Governance/Governor.sol/Governor.json ../app/frontend/contracts/src/abis/
-cp -fv artifacts/contracts/NetEmissionsTokenNetwork.sol/NetEmissionsTokenNetwork.json ../app/frontend/contracts/src/abis/
-echo "Regen ABI from artifacts/contracts/NetEmissionsTokenNetwork.sol/NetEmissionsTokenNetwork.json into ../fabric/typescript_app/src/static/contract-NetEmissionsTokenNetwork.json using jq ..."
-jq .abi artifacts/contracts/NetEmissionsTokenNetwork.sol/NetEmissionsTokenNetwork.json > ../fabric/typescript_app/src/static/contract-NetEmissionsTokenNetwork.json
+cp -fv artifacts/solidity/Governance/DAOToken.sol/DAOToken.json ../app/frontend/contracts/src/abis/
+cp -fv artifacts/solidity/Governance/Governor.sol/Governor.json ../app/frontend/contracts/src/abis/
+cp -fv artifacts/solidity/NetEmissionsTokenNetwork.sol/NetEmissionsTokenNetwork.json ../app/frontend/contracts/src/abis/
+echo "Regen ABI from artifacts/solidity/NetEmissionsTokenNetwork.sol/NetEmissionsTokenNetwork.json into ../fabric/typescript_app/src/static/contract-NetEmissionsTokenNetwork.json using jq ..."
+jq .abi artifacts/solidity/NetEmissionsTokenNetwork.sol/NetEmissionsTokenNetwork.json > ../fabric/typescript_app/src/static/contract-NetEmissionsTokenNetwork.json
 cp ../fabric/typescript_app/src/static/contract-NetEmissionsTokenNetwork.json ../lib/blockchain-gateway/src/static/
