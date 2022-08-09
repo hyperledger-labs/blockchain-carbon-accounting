1f649c9e3a590264d74fb02627874feab01e4b10
Jameson Hodge <contact@jamesonhodge.com>
2022-08-09 16:25:27 -0400

    re-add OZ upgrades package for CarbonTracker; update test addresses
    Signed-off-by: Jameson Hodge <contact@jamesonhodge.com>

--- app/frontend/contracts/src/addresses.ts
+++ app/frontend/contracts/src/addresses.ts
@@ -6,10 +6,10 @@ const networksAndAddresses = {
   hardhat: {
     network: "Hardhat Network",
     tokenNetwork: {
-      address: "0x610178dA211FEF7D417bC0e6FeD39F05609AD788", // default Hardhat Network deployment address with npx hardhat node
+      address: "0x2279B7A0a67DB372996a5FaB50D91eAA73d2eBe6", // default Hardhat Network deployment address with npx hardhat node
     },
     carbonTracker: {
-      address: "0x0dcd1bf9a1b36ce34237eeafef220932846bcd82", // default Hardhat Network deployment address with npx hardhat node
+      address: "0xA51c1fc2f0D1a1b8494Ed1FE312d7C3a78Ed91C0", // default Hardhat Network deployment address with npx hardhat node
     },
     dao: {
       governor: {
